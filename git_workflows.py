4d8efaf1443dc4f8f98ef897b93606e0ea5c03dd
ian mulvany <ian@mulvany.net>
2013-01-08 18:36:54 +0000

    added optparse, and naming conventions

--- git_workflows.py
+++ git_workflows.py
@@ -2,29 +2,53 @@ from git import *
 import glob
 import shutil
 
-xml_repo_base = "/Users/ian/code/public-code/elife-articles"
-pdf_repo_base = "/Users/ian/code/public-code/elife-pdfs"
+XML_REPO_BASE = "/Users/ian/code/public-code/elife-articles"
+PDF_REPO_BASE = "/Users/ian/code/public-code/elife-pdfs"
 
 xml_files = glob.glob("*.xml")
-pdf_files = glob.glob("*.pdf")
-print xml_files
 
-repo = Repo(pdf_repo_base)
-dirty = repo.is_dirty()
-if not dirty:
-	print "I'm clean!!"
-	print "moving files into the repo"
-	# move the files into the repo
-	for f in pdf_files:
-		shutil.move(f, pdf_repo_base + "/" + f)
-		print "moved ", f 
-	# add the files to the repo, commit and push to github
-	git = repo.git
-	git.add("*.pdf")
-	print "adding pdf files to repo ..."
-	print "comitting ..."
-	git.commit(m="new batch")
-	print "committed!"
-	print "pushing to github!"
-	git.push()
-	print "pushed !"
+def move_files_into_repo(files, path_to_repo):
+    for filename in files:
+        shutil.move(filename, path_to_repo + "/" + f)   
+
+def set_repo(repo_base):
+    repo = Repo(repo_base)
+    return repo 
+
+def update_local_and_remote(repo):
+    git = repo.git
+    git.add("*")
+    print "adding pdf files to repo ..."
+    print "comitting ..."
+    git.commit(m="new batch")
+    print "committed!"
+    print "pushing to github!"
+    git.push()
+    print "pushed !"
+
+if __name__ == "__main__":
+    # Add options
+    parser = OptionParser()
+    parser.add_option("-l", "--list", action="store_true", dest="list", help="lists new files to be added to the repo", default="true")
+    parser.add_option("-a", "--add", action="store_false", dest="list", help="add files to the repo, and commit")
+    parser.add_option("-t", "--type", dest="type", help="pick file types", default="xml")
+    (options, args) = parser.parse_args()
+
+    if options.type == "pdf":
+        files = glob.glob("*.pdf")
+        repo_base = PDF_REPO_BASE
+    else:
+        files = glob.glob("*.xml")
+        repo_base = XML_REPO_BASE
+
+    if options.list: 
+        print "I'm going to list the files"
+        print files
+    else:
+        print "I'm going to move the files, and commit the repo"
+        repo = set_repo(repo_base)
+        #move_files_into_repo(repo_base)
+        if repo_base.is_dirty():
+            raise Exception("repo is dirty, aborting")
+        else:
+            print "OK, things look OK for now"
