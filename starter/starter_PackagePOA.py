631f67df8fa177c1f7b7dfa8099472b05661e837
Graham Nott <gnott@starglobal.ca>
2014-03-31 17:02:39 -0700

    Simple PackagePOA starter object.

---
+++ starter/starter_PackagePOA.py
@@ -0,0 +1,84 @@
+import os
+# Add parent directory for imports, so activity classes can use elife-api-prototype
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir)
+
+import boto.swf
+import settings as settingsLib
+import log
+import json
+import random
+import datetime
+import os
+from optparse import OptionParser
+
+import provider.simpleDB as dblib
+
+"""
+Amazon SWF PublishJPG starter
+"""
+
+class starter_PackagePOA():
+
+    def start(self, ENV = "dev", document = None):
+        # Specify run environment settings
+        settings = settingsLib.get_settings(ENV)
+        
+        # Log
+        identity = "starter_%s" % int(random.random() * 1000)
+        logFile = "starter.log"
+        #logFile = None
+        logger = log.logger(logFile, settings.setLevel, identity)
+        
+        # Simple connect
+        conn = boto.swf.layer1.Layer1(settings.aws_access_key_id, settings.aws_secret_access_key)
+    
+        if(document):
+
+            doc = {}
+            doc["document"] = document
+
+            # Get a unique id from the document name for the workflow_id
+            id_string = None
+            try:
+                id_string = document.split("_")[0]
+            except:
+                id_string = "000"
+    
+            # Start a workflow execution
+            workflow_id = "PackagePOA_%s" % (id_string)
+            workflow_name = "PackagePOA"
+            workflow_version = "1"
+            child_policy = None
+            execution_start_to_close_timeout = None
+            input = '{"data": ' + json.dumps(doc) + '}'
+    
+            try:
+                response = conn.start_workflow_execution(settings.domain, workflow_id, workflow_name, workflow_version, settings.default_task_list, child_policy, execution_start_to_close_timeout, input)
+    
+                logger.info('got response: \n%s' % json.dumps(response, sort_keys=True, indent=4))
+                
+            except boto.swf.exceptions.SWFWorkflowExecutionAlreadyStartedError:
+                # There is already a running workflow with that ID, cannot start another
+                message = 'SWFWorkflowExecutionAlreadyStartedError: There is already a running workflow with ID %s' % workflow_id
+                print message
+                logger.info(message)
+
+if __name__ == "__main__":
+
+    document = None
+    
+    # Add options
+    parser = OptionParser()
+    parser.add_option("-e", "--env", default="dev", action="store", type="string", dest="env", help="set the environment to run, either dev or live")
+    parser.add_option("-f", "--file", default=None, action="store", type="string", dest="document", help="specify the S3 object name of the POA zip file")
+    
+    (options, args) = parser.parse_args()
+    if options.env: 
+        ENV = options.env
+    if options.document:
+        document = options.document
+
+    o = starter_PackagePOA()
+
+    o.start(ENV, document = document)
