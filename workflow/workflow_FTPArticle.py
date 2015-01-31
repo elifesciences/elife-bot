e2947dcc25b1e253347465469fca6d2cf2584610
Graham Nott <gnott@starglobal.ca>
2015-01-31 10:14:07 -0800

    FTPArticle set heartbeat timeout to 30 minutes, same as the activity timeout. Large articles (600+ MB) are not transferring in time.

--- workflow/workflow_FTPArticle.py
+++ workflow/workflow_FTPArticle.py
@@ -56,7 +56,7 @@ class workflow_FTPArticle(workflow.workflow):
 					"version": "1",
 					"input": data,
 					"control": None,
-					"heartbeat_timeout": 60*15,
+					"heartbeat_timeout": 60*30,
 					"schedule_to_close_timeout": 60*30,
 					"schedule_to_start_timeout": 300,
 					"start_to_close_timeout": 60*30
