3f8a18f422d6f6c6f28572a494e3de8d0adfdd49
Graham Nott <gnott@starglobal.ca>
2015-03-18 15:45:45 -0700

    Getting the article published date from the data in the S3 bucket folders.

--- tests/features/article_steps.py
+++ tests/features/article_steps.py
@@ -2,6 +2,7 @@ from lettuce import *
 import activity
 import json
 import datetime
+import time
 import os
 import provider.article as articlelib
 
@@ -230,3 +231,74 @@ def i_get_was_ever_poa_is_true(step, was_ever_poa):
   if was_ever_poa == "False": was_ever_poa = False
   assert world.was_ever_poa == was_ever_poa, \
     "Got was_ever_poa %s" % world.was_ever_poa
+  
+@step(u'I have the pub event type (\S+)')
+def i_have_the_pub_event_type_pub_event_type(step,  pub_event_type):
+  world.pub_event_type = pub_event_type
+  assert world.pub_event_type is not None, \
+    "Got pub_event_type %s" % world.pub_event_type
+  
+@step(u'I get article bucket published dates using the article provider')
+def i_get_article_bucket_published_dates_using_the_article_provider(step):
+  force = False
+  world.article_bucket_published_dates = world.article.get_article_bucket_published_dates(
+    force, world.folder_names, world.s3_key_names)
+  assert world.article_bucket_published_dates is not None, \
+    "Got article_bucket_published_dates %s" % json.dumps(world.article_bucket_published_dates)
+
+@step(u'I get article bucket pub date using the article provider')
+def i_get_article_bucket_pub_date_using_the_article_provider(step):
+  world.pub_date = world.article.get_article_bucket_pub_date(world.doi, world.pub_event_type)
+  #assert world.pub_date is not None, \
+  #  "Got pub_date %s" % world.pub_date
+  
+@step(u'I get the date string from the pub date')
+def i_get_the_date_string_from_the_pub_date(step):
+  if world.pub_date is None:
+    world.date_string = None
+  else:
+    world.date_string = time.strftime(world.date_format, world.pub_date)
+    assert world.date_string is not None, \
+      "Got date_string %s" % world.date_string
+  
+@step(u'I get the date string (.*)')
+def i_get_the_date_string(step, date_string):
+  if date_string == "None": date_string = None
+  assert world.date_string == date_string, \
+    "Got date_string %s" % world.date_string
+
+@step(u'I have an s3 key name (.*)')
+def i_have_an_s3_key_name_s3_key_name(step, s3_key_name):
+  world.s3_key_name = s3_key_name
+  assert world.s3_key_name is not None, \
+    "Got s3_key_name %s" % world.s3_key_name
+    
+@step(u'I have the prefix (\S+)')
+def i_have_the_prefix_prefix(step, prefix):
+  world.prefix = prefix
+  assert world.prefix is not None, \
+    "Got prefix %s" % world.prefix
+    
+@step(u'I get doi id from poa s3 key name using the article provider')
+def i_get_doi_id_from_poa_s3_key_name_using_the_article_provider(step):
+  world.doi_id = world.article.get_doi_id_from_poa_s3_key_name(world.s3_key_name)
+  assert world.doi_id is not None, \
+    "Got doi_id %s" % world.doi_id
+    
+@step(u'I get doi id from s3 key name using the article provider')
+def i_get_doi_id_from_s3_key_name_using_the_article_provider(step):
+  world.doi_id = world.article.get_doi_id_from_s3_key_name(world.s3_key_name)
+  assert world.doi_id is not None, \
+    "Got doi_id %s" % world.doi_id
+    
+@step(u'I get doi_id (\d+)')
+def i_get_doi_id_doi_id(step, doi_id):
+  assert world.doi_id == int(doi_id), \
+    "Got doi_id %s" % world.doi_id
+  
+@step(u'I get date string from s3 key name using the article provider')
+def i_get_date_string_from_s3_key_name_using_the_article_provider(step):
+  world.date_string = world.article.get_date_string_from_s3_key_name(world.s3_key_name, world.prefix)
+  assert world.date_string is not None, \
+    "Got date_string %s" % world.date_string
+    
