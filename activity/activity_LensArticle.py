d59d73f5ff4bec741d43f30a7ca5026a7c3a8db3
Graham Nott <gnott@starglobal.ca>
2015-04-02 16:07:42 -0700

    Remove use of filesystem provider, and handle unicodeencodeerror, but unicode titles are still not perfect.

--- activity/activity_LensArticle.py
+++ activity/activity_LensArticle.py
@@ -6,13 +6,13 @@ import calendar
 import time
 import requests
 import os
+import codecs
 
 import activity
 
 import boto.s3
 from boto.s3.connection import S3Connection
 
-import provider.filesystem as fslib
 import provider.templates as templatelib
 import provider.article as articlelib
 
@@ -32,9 +32,6 @@ class activity_LensArticle(activity.activity):
 		self.default_task_schedule_to_start_timeout = 30
 		self.default_task_start_to_close_timeout= 60*5
 		self.description = "Create a lens article index.html page for the particular article."
-		
-		# Create the filesystem provider
-		self.fs = fslib.Filesystem(self.get_tmp_dir())
 		
 		# Templates provider
 		self.templates = templatelib.Templates(settings, self.get_tmp_dir())
@@ -64,7 +61,7 @@ class activity_LensArticle(activity.activity):
 		article_html = self.get_article_html(from_dir = self.from_dir, article = self.article)
 		
 		# Write the document to disk first
-		self.fs.write_content_to_document(article_html, filename)
+		filename_plus_path = self.write_html_file(article_html, filename)
 		
 		# Now, set the S3 object to the contents of the filename
 		# Connect to S3
@@ -74,7 +71,7 @@ class activity_LensArticle(activity.activity):
 		bucket = s3_conn.lookup(bucket_name)
 		s3key = boto.s3.key.Key(bucket)
 		s3key.key = article_s3key
-		s3key.set_contents_from_filename(self.get_document(), replace=True)
+		s3key.set_contents_from_filename(filename_plus_path, replace=True)
 		
 		if(self.logger):
 			self.logger.info('LensArticle created for: %s' % article_s3key)
@@ -103,13 +100,15 @@ class activity_LensArticle(activity.activity):
 
 		return article_html
 		
-	def get_document(self):
+	def write_html_file(self, article_html, filename):
 		"""
-		Exposed for running tests
+		Write the HTML to the disk
 		"""
-		if(self.fs.tmp_dir):
-			full_filename = self.fs.tmp_dir + os.sep + self.fs.get_document()
-		else:
-			full_filename = self.fs.get_document()
 		
-		return full_filename
+		filename_plus_path = self.get_tmp_dir() + os.sep + filename
+		mode = "w"
+		f = codecs.open(filename_plus_path, 'w', encoding='utf8')
+		f.write(article_html)
+		f.close()
+		
+		return filename_plus_path
