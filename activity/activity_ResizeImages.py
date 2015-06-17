b2e47062c755a8422d64d9409dd2d3ce2c60b49c
John Root <john.root@digirati.co.uk>
2015-06-17 21:23:26 +0100

    added activity_ResizeImages and formats.yaml

---
+++ activity/activity_ResizeImages.py
@@ -0,0 +1,133 @@
+import random
+from S3utility.s3_notification_info import S3NotificationInfo
+import activity
+import json
+from boto.s3.connection import S3Connection
+import log
+import boto.swf
+from provider.article_structure import ArticleInfo
+from wand.image import Image
+import StringIO
+import settings as settings_lib
+import yaml
+
+
+"""
+ResizeImages.py activity
+"""
+
+class activity_ResizeImages(activity.activity):
+    def __init__(self, settings, logger, conn=None, token=None, activity_task=None):
+        activity.activity.__init__(self, settings, logger, conn, token, activity_task)
+
+        self.name = "ResizeImages"
+        self.version = "1"
+
+        # standard bot activity parameters
+        self.default_task_heartbeat_timeout = 30
+        self.default_task_schedule_to_close_timeout = 60 * 5
+        self.default_task_schedule_to_start_timeout = 30
+        self.default_task_start_to_close_timeout = 60 * 5
+        self.description = "Resize article images"
+        self.logger = logger
+        self.formats = self.load_formats()
+        # TODO : better exception handling
+
+    def do_activity(self, data=None):
+        """
+        Do the work
+        """
+        if self.logger:
+            self.logger.info('data: %s' % json.dumps(data, sort_keys=True, indent=4))
+
+        # extract S3 notification information
+        info = S3NotificationInfo.from_dict(data)
+        zip_file_name = info.file_name
+
+        if self.logger:
+            self.logger.info("Converting images for article %s" % ",".join(map(str, zip_file_name)))
+
+        # get information on files in the expanded article bucket for notified zip file
+        bucket, file_infos = self.get_file_infos(zip_file_name)
+
+        for file_info in file_infos:
+            key = bucket.get_key(file_info.key)
+            # see : http://stackoverflow.com/questions/9954521/s3-boto-list-keys-sometimes-returns-directory-key
+            if not key.name.endswith("/"):
+                # process each key in the folder
+                self.process_key(key)
+        return True
+
+    def get_file_infos(self, zip_file_name):
+        # connect to S3 and obtain the expanded article bucket
+        conn = S3Connection(self.settings.aws_access_key_id, self.settings.aws_secret_access_key)
+        bucket = conn.get_bucket(self.settings.expanded_article_bucket)
+
+        # determine the correct expanded article folder for the zip file that was notified
+        folder = zip_file_name.rsplit('.', 1)[0]
+
+        # get the keys for the files in the folder and return along with a reference to the bucket
+        file_infos = bucket.list(folder + "/", "/")
+        return bucket, file_infos
+
+    def process_key(self, key):
+        # determine filename (without folder) and obtain ArticleInfo instance
+        filename = key.name.rsplit('/', 1)[1]
+        info = ArticleInfo(filename)
+
+        # see if we have any formats available for the file_type of this file
+        formats = self.get_formats(info.file_type)
+        if formats is not None:
+            # generate images for relevant formats
+            self.generate_images(formats, key, info)
+
+    def get_formats(self, file_type):
+        # look up file_type in pre-parsed formats
+        if file_type in self.formats:
+            return self.formats[file_type]
+        return None
+
+    def generate_images(self, formats, key, info):
+        fp = StringIO.StringIO()
+        key.get_file(fp)
+        fp.seek(0)
+        try:
+            with Image(file=fp) as img:
+                pass
+        except Exception as e:
+            print e
+
+    def load_formats(self):
+        # load the formats from the YAML file
+        stream = file('formats.yaml', 'r')
+        formats = yaml.load(stream)
+        return formats
+
+
+def main(args):
+
+    """
+    This sets up dummy SWF activity data, creates an instance of this activity and runs it only for
+    testing and debugging. This activity would usually be executed by worker.py
+    """
+
+    data = {
+        'file_name': 'elife-00012-vor-r1.zip',
+        'event_name': None,
+        'event_time': None,
+        'bucket_name': None,
+        'file_etag': None,
+        'file_size': None
+    }
+
+    settings = settings_lib.get_settings('exp')
+    identity = "resize_%s" % int(random.random() * 1000)
+    log_file = "worker.log"
+    logger = log.logger(log_file, settings.setLevel, identity)
+    conn = boto.swf.layer1.Layer1(settings.aws_access_key_id, settings.aws_secret_access_key)
+    act = activity_ResizeImages(settings, logger, conn=conn)
+    act.do_activity(data)
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
