aa56a75f4bf85e3d6db5c050236679b4407b1fce
John Root <john.root@digirati.co.uk>
2015-06-18 17:49:29 +0100

    updates to image resizing

--- activity/activity_ResizeImages.py
+++ activity/activity_ResizeImages.py
@@ -4,6 +4,7 @@ import StringIO
 from S3utility.s3_notification_info import S3NotificationInfo
 import activity
 from boto.s3.connection import S3Connection
+from boto.s3.key import Key
 import log
 import boto.swf
 from provider.article_structure import ArticleInfo
@@ -14,6 +15,7 @@ import provider.imageresize as resizer
 """
 ResizeImages.py activity
 """
+
 
 class activity_ResizeImages(activity.activity):
     def __init__(self, settings, logger, conn=None, token=None, activity_task=None):
@@ -54,14 +56,15 @@ class activity_ResizeImages(activity.activity):
             # see : http://stackoverflow.com/questions/9954521/s3-boto-list-keys-sometimes-returns-directory-key
             if not key.name.endswith("/"):
                 # process each key in the folder
-                self.process_key(key)
+                self.process_key(key, zip_file_name)
 
         return True
 
     def get_file_infos(self, zip_file_name):
         # connect to S3 and obtain the expanded article bucket
-        conn = S3Connection(self.settings.aws_access_key_id, self.settings.aws_secret_access_key)
-        bucket = conn.get_bucket(self.settings.expanded_article_bucket)
+        self.conn = S3Connection(self.settings.aws_access_key_id, self.settings.aws_secret_access_key,
+                                 host=self.settings.s3_hostname)
+        bucket = self.conn.get_bucket(self.settings.expanded_article_bucket)
 
         # determine the correct expanded article folder for the zip file that was notified
         folder = zip_file_name.rsplit('.', 1)[0]
@@ -70,7 +73,7 @@ class activity_ResizeImages(activity.activity):
         file_infos = bucket.list(folder + "/", "/")
         return bucket, file_infos
 
-    def process_key(self, key):
+    def process_key(self, key, zip_file_name):
         # determine filename (without folder) and obtain ArticleInfo instance
         filename = key.name.rsplit('/', 1)[1]
         info = ArticleInfo(filename)
@@ -81,7 +84,7 @@ class activity_ResizeImages(activity.activity):
             # generate images for relevant formats
             fp = StringIO.StringIO()
             key.get_file(fp)
-            self.generate_images(formats, fp, info)
+            self.generate_images(formats, fp, info, zip_file_name)
 
     def get_formats(self, file_type):
         # look up file_type in pre-parsed formats
@@ -89,16 +92,29 @@ class activity_ResizeImages(activity.activity):
             return self.formats[file_type]
         return None
 
-    def generate_images(self, formats, fp, info):
+    def generate_images(self, formats, fp, info, zip_file_name):
         # delegate this to module
-        for format in formats:
-            fp.seek(0)  # rewind the tape
-            filename, image = resizer.resize(format, fp, info)
-            print "resized : " + filename + "filesize" + str(image.len)
-            # TODO : write the image to the 'CDN' (or destinaton bucket)
+        try:
+            for format_spec_name in formats:
+                fp.seek(0)  # rewind the tape
+                filename, image = resizer.resize(formats[format_spec_name], fp, info)
+                self.store_in_cdn(filename, image, zip_file_name)
+        finally:
+            fp.close()
+
+    def store_in_cdn(self, filename, image, zip_file_name):
+        # for now we'l use an S3 bucket
+        try:
+            cdn_bucket = self.conn.get_bucket(self.settings.article_cdn_bucket)
+            key = Key(cdn_bucket)
+            key.key = zip_file_name.rsplit('.', 1)[0] + "/" + filename
+            image.seek(0)
+            key.set_contents_from_file(image)
+        finally:
+            image.close()
 
     def load_formats(self):
-        # load the formats from the YAML file
+        # load the formats fro m the YAML file
         stream = file('formats.yaml', 'r')
         formats = yaml.load(stream)
         return formats
@@ -112,7 +128,7 @@ def main(args):
     """
 
     data = {
-        'file_name': 'elife-00012-vor-r1.zip',
+        'file_name': 'elife-00012-vor-v1.zip',
         'event_name': None,
         'event_time': None,
         'bucket_name': None,
