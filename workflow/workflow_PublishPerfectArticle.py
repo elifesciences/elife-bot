7b003c8fbfdbbd4d3f5247bbac9654b285df0eff
Graham Nott <gnott@starglobal.ca>
2016-02-09 15:37:41 -0800

    ResizeImages extend the timeout from 5 minutes to 8 minutes when part of the PublishPerfectArticle workflow.

--- workflow/workflow_PublishPerfectArticle.py
+++ workflow/workflow_PublishPerfectArticle.py
@@ -94,10 +94,10 @@ class workflow_PublishPerfectArticle(workflow.workflow):
                         "version": "1",
                         "input": data,
                         "control": None,
-                        "heartbeat_timeout": 60 * 5,
-                        "schedule_to_close_timeout": 60 * 5,
+                        "heartbeat_timeout": 60 * 8,
+                        "schedule_to_close_timeout": 60 * 8,
                         "schedule_to_start_timeout": 300,
-                        "start_to_close_timeout": 60 * 5
+                        "start_to_close_timeout": 60 * 8
                     },
                     {
                         "activity_type": "DepositAssets",
