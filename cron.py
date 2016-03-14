3206c6d7ad2d6685d64cfa7401ec4f00bdec8187
Graham Nott <gnott@starglobal.ca>
2016-03-14 14:49:36 -0700

    Remove old supplemental zip file workflow code.

--- cron.py
+++ cron.py
@@ -105,13 +105,7 @@ def run_cron(ENV = "dev"):
     #  starter_name  = "cron_NewS3XML",
     #  workflow_id   = "cron_NewS3XML",
     #  start_seconds = 60*31)
-            
-    #workflow_conditional_start(
-    #  ENV           = ENV,
-    #  starter_name  = "cron_NewS3Suppl",
-    #  workflow_id   = "cron_NewS3Suppl",
-    #  start_seconds = 60*31)
-    
+                
     #workflow_conditional_start(
     #  ENV           = ENV,
     #  starter_name  = "cron_NewS3JPG",
@@ -255,7 +249,6 @@ def workflow_conditional_start(ENV, starter_name, start_seconds, data = None, wo
 
     elif(starter_name == "cron_NewS3XML"
       or starter_name == "cron_FiveMinute"
-      or starter_name == "cron_NewS3Suppl"
       or starter_name == "cron_NewS3JPG"
       or starter_name == "starter_PublishPOA"
       or starter_name == "cron_NewS3POA"
