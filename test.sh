1013888dc8a7d5dccb625c0dac9aed85da8c53f5
Giorgio Sironi <g.sironi@elifesciences.org>
2016-05-10 09:12:57 +0000

    Running both lettuce and unittest, stopping at the first error

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #±/bin/bash
 set -e
-source ../venv/bin/activate
+source venv/bin/activate
+cd tests/
 for i in `ls features/*.feature`; do echo $i; lettuce $i -v 1; done
+cd -
+bash ./travis-test.sh
+ 
 
