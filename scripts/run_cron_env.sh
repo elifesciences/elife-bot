6887f71c9a2ef8a64e8e346cca59e3045c6c312a
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-27 15:05:22 +0000

    Updated documentation of script

--- scripts/run_cron_env.sh
+++ scripts/run_cron_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Run from the parent directory e.g. "./scripts/run_cron.sh live"
+# Run from the parent directory e.g. "./scripts/run_cron_env.sh live"
 alias ..='cd ..'
 source /opt/elife-bot/venv/bin/activate && python cron.py -e $1 > /dev/null
