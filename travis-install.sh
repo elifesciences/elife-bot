9aef4adfca4d7f2f3330904d75dd5091b45fd535
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-28 14:34:13 +0000

    Travis needs a settings.py to be installed, as there is no provisioning happening there

--- travis-install.sh
+++ travis-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e # everything must succeed.
+ln -s settings-example.py settings.py
 pip install -r requirements.txt
 git clone https://github.com/elifesciences/elife-poa-xml-generation.git ../elife-poa-xml-generation
 cp ../elife-poa-xml-generation/example-settings.py ../elife-poa-xml-generation/settings.py
