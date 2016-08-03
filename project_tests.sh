54c7bc505cdf7dd6165ad5283e839fca88f246e5
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-03 09:38:59 +0100

    Default verbosity for lettuce

--- project_tests.sh
+++ project_tests.sh
@@ -2,7 +2,7 @@
 set -e
 source venv/bin/activate
 cd tests/
-for i in `ls features/*.feature`; do echo $i; lettuce $i -v 1; done
+for i in `ls features/*.feature`; do echo $i; lettuce $i; done
 cd -
 rm -f build/junit.xml
 bash ./travis-test.sh
