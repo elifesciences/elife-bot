a65a94414bbaecd7ad10431f796478f152391174
jennifer <jenniferstrej@gmail.com>
2016-10-10 17:42:07 +0100

    Fix in PreparePostEIF test

--- tests/activity/test_activity_prepare_post_eif.py
+++ tests/activity/test_activity_prepare_post_eif.py
@@ -1,7 +1,7 @@
 import unittest
 from activity.activity_PreparePostEIF import activity_PreparePostEIF
 import settings_mock
-import test_activity_data as data
+import test_activity_data as test_data
 from mock import mock, patch
 from classes_mock import FakeSession
 from classes_mock import FakeSQSConn
@@ -14,7 +14,6 @@ from testfixtures import TempDirectory
 import json
 import base64
 
-
 class tests_PreparePostEIF(unittest.TestCase):
     def setUp(self):
         self.activity_PreparePostEIF = activity_PreparePostEIF(
@@ -23,14 +22,14 @@ class tests_PreparePostEIF(unittest.TestCase):
         self.do_activity_passes = []
         activity_pass = {
             "update_date": "2012-12-13T00:00:00Z",
-            "message": data.PreparePostEIF_message,
-            "expected": data.PreparePostEIF_json_output_return_example
+            "message": test_data.PreparePostEIF_message,
+            "expected": test_data.PreparePostEIF_json_output_return_example
         }
-        activity_pass = {
-            "update_date": None,
-            "message": data.PreparePostEIF_message_no_update_date,
-            "expected": data.PreparePostEIF_json_output_return_example_no_update_date
-        }
+        # activity_pass = {
+        #     "update_date": None,
+        #     "message": test_data.PreparePostEIF_message_no_update_date,
+        #     "expected": test_data.PreparePostEIF_json_output_return_example_no_update_date
+        # }
         self.do_activity_passes.append(activity_pass)
 
     def tearDown(self):
@@ -45,10 +44,10 @@ class tests_PreparePostEIF(unittest.TestCase):
                       mock_sqs_message, mock_sqs_connect):
         directory = TempDirectory()
 
-        for test_data in self.do_activity_passes:
+        for testdata in self.do_activity_passes:
 
-            fake_session_mock.return_value = FakeSession(data.PreparePost_session_example(
-                test_data["update_date"]))
+            fake_session_mock.return_value = FakeSession(test_data.PreparePost_session_example(
+                testdata["update_date"]))
             mock_sqs_connect.return_value = FakeSQSConn(directory)
             mock_sqs_message.return_value = FakeSQSMessage(directory)
             fake_s3_mock.return_value = FakeS3Connection()
@@ -56,16 +55,16 @@ class tests_PreparePostEIF(unittest.TestCase):
             self.activity_PreparePostEIF.set_monitor_property = mock.MagicMock()
             self.activity_PreparePostEIF.emit_monitor_event = mock.MagicMock()
 
-            success = self.activity_PreparePostEIF.do_activity(data.PreparePostEIF_data)
+            success = self.activity_PreparePostEIF.do_activity(test_data.PreparePostEIF_data)
 
             fake_sqs_queue = FakeSQSQueue(directory)
-            data_written_in_test_queue = fake_sqs_queue.read(data.PreparePostEIF_test_dir)
+            data_written_in_test_queue = fake_sqs_queue.read(test_data.PreparePostEIF_test_dir)
 
             self.assertEqual(True, success)
-            self.assertEqual(json.dumps(test_data["message"]), data_written_in_test_queue)
+            self.assertEqual(json.dumps(testdata["message"]), data_written_in_test_queue)
 
-            output_json = json.loads(directory.read(data.PreparePostEIF_test_dir))
-            expected = test_data["expected"]
+            output_json = json.loads(directory.read(test_data.PreparePostEIF_test_dir))
+            expected = testdata["expected"]
             self.assertDictEqual(output_json, expected)
 
 if __name__ == '__main__':
