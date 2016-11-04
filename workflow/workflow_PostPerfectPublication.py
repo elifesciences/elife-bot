14873e293247324c97af9bf5e8ca9520fe3c7cb9
jennifer <jenniferstrej@gmail.com>
2016-11-04 18:47:59 +0000

    Added UpdateRepository at the end of PostPerfectPublication workflow

--- workflow/workflow_PostPerfectPublication.py
+++ workflow/workflow_PostPerfectPublication.py
@@ -109,6 +109,17 @@ class workflow_PostPerfectPublication(workflow.workflow):
                         "schedule_to_close_timeout": 60 * 5,
                         "schedule_to_start_timeout": 300,
                         "start_to_close_timeout": 60 * 5
+                    },
+                    {
+                        "activity_type": "UpdateRepository",
+                        "activity_id": "UpdateRepository",
+                        "version": "1",
+                        "input": data,
+                        "control": None,
+                        "heartbeat_timeout": 60 * 5,
+                        "schedule_to_close_timeout": 60 * 5,
+                        "schedule_to_start_timeout": 300,
+                        "start_to_close_timeout": 60 * 5
                     }
                 ],
 
