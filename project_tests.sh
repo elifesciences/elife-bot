184647e697922a488dfe29701729e9d3651af3ef
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-11 13:50:20 +0000

    Checking sanity of scripts with Pylint
    This catches undefined variables/modules and similar errors

--- project_tests.sh
+++ project_tests.sh
@@ -1,10 +1,11 @@
-#±/bin/bash
+#!/bin/bash
 set -e
 source venv/bin/activate
 cd tests/
 for i in `ls features/*.feature`; do echo $i; lettuce $i; done
 cd -
 rm -f build/junit.xml
+./lint.sh
 bash ./travis-test.sh
  
 
