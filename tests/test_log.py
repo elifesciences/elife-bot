f8acc7d3df4a4e99ca5014ce954e8e01adb15f1c
Giorgio Sironi <g.sironi@elifesciences.org>
2016-12-01 16:09:39 +0000

    Added unit test for log usage

--- tests/test_log.py
+++ tests/test_log.py
@@ -3,9 +3,11 @@ import log
 import unittest
 
 class TestLog(unittest.TestCase):
-    def test_logger_creation(self):
+    def test_logger_creation_and_usage(self):
         logger = log.logger('worker.log', 'INFO', 'worker_123')
         self.assertIsInstance(logger, logging.Logger)
+        logger.info("Test info message")
+        logger.error("Test error message")
 
     def test_identity_generation(self):
         self.assertRegexpMatches(log.identity('worker'), '^worker_[0-9]+$')
