e6984a2719c05abccae2e5d8e66c5eb1a99bf5ab
Giorgio Sironi <g.sironi@elifesciences.org>
2017-03-28 14:46:06 +0100

    Added unit test for excluing figures .jpg

--- tests/provider/test_article_structure.py
+++ tests/provider/test_article_structure.py
@@ -110,8 +110,10 @@ class TestArticleStructure(unittest.TestCase):
         self.assertListEqual(article_structure.get_original_files(files), expected)
 
     def test_get_figures_for_iiif(self):
+        "Only .tif of original figures"
         files = ['elife-00666-app1-fig1-figsupp1-v1.tif',
                  'elife-00666-fig2-figsupp2-v1.tif',
+                 'elife-00666-fig2-figsupp2-v1.jpg',
                  'elife-00666-inf001-v1.jpg',
                  'elife-00666-inf001-v1-80w.jpg',
                  'elife-00666-table3-data1-v1.xlsx',
