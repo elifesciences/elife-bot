2a937362842c9dd4c6a19715f47bdfb696f74ab8
jennifer <jenniferstrej@gmail.com>
2017-03-30 18:16:05 +0100

    CopyGlencoeStillImages to run standalone
    test

---
+++ starter/starter_CopyGlencoeStillImages.py
@@ -0,0 +1,99 @@
+import os
+import boto.swf.exceptions
+
+# Add parent directory for imports
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0, parentdir)
+
+import boto.swf
+import log
+import json
+from argparse import ArgumentParser
+
+import starter_helper as helper
+from starter_helper import NullRequiredDataException
+
+"""
+Amazon SWF CopyGlencoeStillImages starter, for copying Glencoe still images to IIIF bucket.
+"""
+
+
+class starter_CopyGlencoeStillImages():
+    def __init__(self):
+        self.const_name = "CopyGlencoeStillImages"
+
+    def start(self, settings, article_id=None, version=None, run=None, standalone=False, standalone_is_poa=False):
+
+        # Log
+        logger = helper.get_starter_logger(settings.setLevel, helper.get_starter_identity(self.const_name))
+
+        if article_id is None:
+            raise NullRequiredDataException("Did not get an article id. Required.")
+
+        info = {
+            'run': run,
+            'standalone': standalone,
+            'standalone_is_poa': standalone_is_poa
+        }
+
+        workflow_id, \
+        workflow_name, \
+        workflow_version, \
+        child_policy, \
+        execution_start_to_close_timeout, \
+        workflow_input = helper.set_workflow_information(self.const_name, "1", None, info, article_id)
+
+        # Simple connect
+        conn = boto.swf.layer1.Layer1(settings.aws_access_key_id, settings.aws_secret_access_key)
+
+        try:
+            response = conn.start_workflow_execution(settings.domain, workflow_id, workflow_name, workflow_version,
+                                                     settings.default_task_list, child_policy,
+                                                     execution_start_to_close_timeout, workflow_input)
+
+            logger.info('got response: \n%s' % json.dumps(response, sort_keys=True, indent=4))
+
+        except NullRequiredDataException as e:
+            logger.exception(e.message)
+            raise
+
+        except boto.swf.exceptions.SWFWorkflowExecutionAlreadyStartedError:
+            # There is already a running workflow with that ID, cannot start another
+            message = 'SWFWorkflowExecutionAlreadyStartedError: There is already a running workflow with ID %s' % workflow_id
+            logger.info(message)
+
+
+def main():
+
+    # Add options
+    parser = ArgumentParser()
+    parser.add_argument("-e", "--env", default="dev", action="store", type="string", dest="env",
+                      help="set the environment to run, e.g. dev, live, prod, end2end")
+    parser.add_argument("-i", "--article-id", default=None, action="store", type="string",
+                      dest="article_id", help="specify the article id to process")
+    parser.add_argument("-i", "--is-poa", default=False, action="store", type="string",
+                      dest="is_poa",
+                      help="True if article is POA. Defaults to False.")
+
+    args = parser.parse_args()
+
+    args = parser.parse_args()
+    ENV = None
+    if args.env:
+        ENV = args.env
+    article_id = None
+    is_poa = False
+    if args.article_id:
+        article_id = args.article_id
+    if args.is_poa:
+        is_poa = args.is_poa
+
+    import settings as settingsLib
+    settings = settingsLib.get_settings(ENV)
+
+    o = starter_CopyGlencoeStillImages()
+
+    o.start(settings=settings, article_id=article_id, standalone=True, standalone_is_poa=is_poa)
+
+if __name__ == "__main__":
+    main()
