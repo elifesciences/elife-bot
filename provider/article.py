79c7ffc367e0e00f7189405a91d7f6f0d10b0247
Graham Nott <gnott@starglobal.ca>
2017-05-02 18:01:43 -0700

    Article provider to download XML from S3 from the new CDN bucket.

--- provider/article.py
+++ provider/article.py
@@ -167,8 +167,8 @@ class article(object):
             return False
 
         # Download XML file via HTTP for now
-        cdn_bucket_name = self.settings.publishing_buckets_prefix + self.settings.ppp_cdn_bucket
-        xml_file_url = ('http://s3-external-1.amazonaws.com/' + cdn_bucket_name + '/'
+        published_bucket_name = self.settings.publishing_buckets_prefix + self.settings.published_bucket
+        xml_file_url = ('http://s3-external-1.amazonaws.com/' + published_bucket_name + '/articles/'
                         + doi_id + '/' + 'elife-' + doi_id + '-v' + str(version) + '.xml')
         xml_filename = xml_file_url.split('/')[-1]
 
