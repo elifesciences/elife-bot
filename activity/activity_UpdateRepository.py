50488bcb979f3a09c550a60b38847e3899511f97
jennifer <jenniferstrej@gmail.com>
2017-05-09 13:44:50 +0100

    Removes reference to elife-published bucket by itself
    second step of:
    1. change `ppp_cdn_bucket` to the new value. the bot will write twice to the same place
    2. change the bot removing every reference to `published_bucket`, which is not necessary anymore
    3. rename the setting `ppp_cdn_bucket` to something longer-lasting (we create a new value, leave both old and new key available for a while, eventually removing the old one)

--- activity/activity_UpdateRepository.py
+++ activity/activity_UpdateRepository.py
@@ -62,10 +62,10 @@ class activity_UpdateRepository(activity.activity):
                 with tempfile.TemporaryFile(mode='r+') as tmp:
                     storage_context = get_storage_context(self.settings)
                     storage_provider = self.settings.storage_provider + "://"
-                    published_bucket = storage_provider + self.settings.publishing_buckets_prefix + \
+                    published_path = storage_provider + self.settings.publishing_buckets_prefix + \
                                        self.settings.ppp_cdn_bucket
 
-                    resource = published_bucket + "/" + s3_file_path
+                    resource = published_path + "/" + s3_file_path
 
                     storage_context.get_resource_to_file(resource, tmp)
 
