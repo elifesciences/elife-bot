602017e40a7696993ca37f5f041273bca53f3149
jennifer <jenniferstrej@gmail.com>
2017-05-16 11:42:54 +0100

    bucket_path

--- provider/article.py
+++ provider/article.py
@@ -168,8 +168,8 @@ class article(object):
             return False
 
         # Download XML file via HTTP for now
-        bucket_name = self.settings.publishing_buckets_prefix + self.settings.ppp_cdn_bucket
-        xml_file_url = ('http://s3-external-1.amazonaws.com/' + bucket_name + '/'
+        bucket_path = self.settings.publishing_buckets_prefix + self.settings.ppp_cdn_bucket
+        xml_file_url = ('http://s3-external-1.amazonaws.com/' + bucket_path + '/'
                         + doi_id + '/' + 'elife-' + doi_id + '-v' + str(version) + '.xml')
         xml_filename = xml_file_url.split('/')[-1]
 
