598c7ef412f2dc21ffa065a9ce08214aa819fb3b
jennifer <jenniferstrej@gmail.com>
2017-05-23 09:58:07 +0100

    retry for verify IIIF endpoint

--- provider/iiif.py
+++ provider/iiif.py
@@ -1,5 +1,8 @@
 import requests
 
+class ShortRetryException(RuntimeError):
+    pass
+
 def endpoint(settings, iiif_path_for_article, figure):
     iiif_path_for_figure = iiif_path_for_article.replace('{article_fig}', figure)
     return settings.path_to_iiif_server + iiif_path_for_figure
@@ -7,10 +10,15 @@ def endpoint(settings, iiif_path_for_article, figure):
 def try_endpoint(endpoint, logger):
     try:
         response = requests.head(endpoint)
+        if response.status_code == 504:
+            raise ShortRetryException("Response code was %s" % response.status_code)
         if response.status_code != 200:
             logger.error("Error status code != 200. Status code: " + str(response.status_code) + " for URL: " + endpoint)
             return False, endpoint
         return True, endpoint
+    except ShortRetryException as e:
+        logger.info('short retry because %s', e)
+        return try_endpoint(endpoint, logger)
     except Exception as e:
         logger.exception(str(e))
         return False, endpoint
