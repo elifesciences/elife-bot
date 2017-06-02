28def8a91a0195231b2501855ca856c0d711bb2d
jennifer <jenniferstrej@gmail.com>
2017-06-02 17:11:29 +0100

    comment clarifying test data

--- tests/activity/test_activity_pub_router_deposit.py
+++ tests/activity/test_activity_pub_router_deposit.py
@@ -8,6 +8,8 @@ class TestPubRouterDeposit(unittest.TestCase):
     def setUp(self):
         self.pubrouterdeposit = activity_PubRouterDeposit(settings_mock, None, None, None, None)
 
+    # input: s3 archive zip file name (name) and date last modified
+    # expected output: file name - highest version file (displayed on -v[number]-) then latest last modified date/time
     @unpack
     @data({"input": [{"name": "elife-16747-vor-v1-20160831000000.zip", "last_modified": "2017-05-18T09:04:11.000Z"},
                     {"name": "elife-16747-vor-v1-20160831132647.zip", "last_modified": "2016-08-31T06:26:56.000Z"}],
