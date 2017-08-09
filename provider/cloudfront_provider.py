a0dd5c85ba302540f5e9038b5c8444ed5712fe6f
jennifer <jenniferstrej@gmail.com>
2017-08-09 14:42:52 +0100

    removes extra brackets

--- provider/cloudfront_provider.py
+++ provider/cloudfront_provider.py
@@ -2,7 +2,7 @@ from boto.cloudfront import CloudFrontConnection
 
 
 def create_invalidation(article, settings):
-    cloudfront = CloudFrontConnection((settings.aws_access_key_id, settings.aws_secret_access_key))
+    cloudfront = CloudFrontConnection(settings.aws_access_key_id, settings.aws_secret_access_key)
     inval_req = cloudfront.create_invalidation_request(settings.cloudfront_distribution_id_cdn,
                                                        "/articles/" + article + "/*")
     assert isinstance(inval_req, cloudfront.invalidation.InvalidationBatch), \
