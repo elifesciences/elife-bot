b084cd3c6bb924dac9f6ef7cee93674a9f55d361
Graham Nott <gnott@starglobal.ca>
2017-08-31 16:38:43 -0700

    article_processing provider, refactored article zip processing functions for reuse.

---
+++ provider/article_processing.py
@@ -0,0 +1,140 @@
+import os
+import shutil
+import dateutil.parser
+from elifetools import xmlio
+
+"""
+Functions for processing article zip and XML for reuse by different activities
+Originally refactoring them from the PMCDeposit activity for reuse into FTPArticle
+"""
+
+def list_dir(dir_name):
+    dir_list = os.listdir(dir_name)
+    dir_list = map(lambda item: dir_name + os.sep + item, dir_list)
+    return dir_list
+
+def file_list(dir_name):
+    dir_list = list_dir(dir_name)
+    return filter(lambda item: os.path.isfile(item), dir_list)
+
+
+def rename_files_remove_version_number(files_dir, output_dir, logger=None):
+    """
+    Rename files to not include the version number, if present
+    Pre-PPP files will not have a version number, for before PPP is launched
+    """
+
+    file_name_map = {}
+
+    # Get a list of all files
+    dirfiles = file_list(files_dir)
+
+    for df in dirfiles:
+        filename = df.split(os.sep)[-1]
+
+        # Get the new file name
+        file_name_map[filename] = None
+
+        # TODO strip the -v1 from it
+        file_extension = filename.split('.')[-1]
+        if '-v' in filename:
+            # Use part before the -v number
+            part_without_version = filename.split('-v')[0]
+        else:
+            # No -v found, use the file name minus the extension
+            part_without_version = ''.join(filename.split('.')[0:-1])
+
+        renamed_filename = part_without_version + '.' + file_extension
+
+        if renamed_filename:
+            file_name_map[filename] = renamed_filename
+        else:
+            if logger:
+                logger.info('there is no renamed file for ' + filename)
+
+    for old_name, new_name in file_name_map.iteritems():
+        if new_name is not None:
+            shutil.move(files_dir + os.sep + old_name, output_dir + os.sep + new_name)
+
+    return file_name_map
+
+
+def convert_xml(xml_file, file_name_map):
+
+    # Register namespaces
+    xmlio.register_xmlns()
+
+    root, doctype_dict = xmlio.parse(xml_file, return_doctype_dict=True)
+
+    # Convert xlink href values
+    total = xmlio.convert_xlink_href(root, file_name_map)
+    # TODO - compare whether all file names were converted
+
+    # Start the file output
+    reparsed_string = xmlio.output(root, type=None, doctype_dict=doctype_dict)
+
+    f = open(xml_file, 'wb')
+    f.write(reparsed_string)
+    f.close()
+
+
+def verify_rename_files(file_name_map):
+    """
+    Each file name as key should have a non None value as its value
+    otherwise the file did not get renamed to something new and the
+    rename file process was not complete
+    """
+    verified = True
+    renamed_list = []
+    not_renamed_list = []
+    for k, v in file_name_map.items():
+        if v is None:
+            verified = False
+            not_renamed_list.append(k)
+        else:
+            renamed_list.append(k)
+
+    return (verified, renamed_list, not_renamed_list)
+
+
+def new_pmc_zip_filename(journal, volume, fid, revision=None):
+    filename = journal
+    filename = filename + '-' + str(volume).zfill(2)
+    filename = filename + '-' + str(fid).zfill(5)
+    if revision:
+        filename = filename + '.r' + str(revision)
+    filename += '.zip'
+    return filename
+
+
+def latest_archive_zip_revision(doi_id, s3_keys, journal, status):
+    """
+    Get the most recent version of the article zip file from the
+    list of bucket key names
+    """
+    s3_key_name = None
+
+    name_prefix_to_match = (journal + '-' + str(doi_id).zfill(5)
+                            + '-' + status + '-v')
+
+    highest = 0
+    for key in s3_keys:
+        if key["name"].startswith(name_prefix_to_match):
+            version_and_date = None
+            try:
+                parts = key["name"].split(name_prefix_to_match)
+                version = parts[1].split('-')[0]
+                date_formatted = dateutil.parser.parse(key["last_modified"])
+                date_part = date_formatted.strftime('%Y%m%d%H%M%S')
+                version_and_date = int(version + date_part)
+            except:
+                pass
+            if version_and_date and version_and_date > highest:
+                s3_key_name = key["name"]
+                highest = version_and_date
+
+    return s3_key_name
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/provider/test_article_processing.py
@@ -0,0 +1,115 @@
+import unittest
+import os
+import zipfile
+from ddt import ddt, data, unpack
+from testfixtures import tempdir
+from testfixtures import TempDirectory
+import provider.article_processing as article_processing
+
+
+@ddt
+class TestArticleProcessing(unittest.TestCase):
+
+    def setUp(self):
+        self.directory = TempDirectory()
+        self.file_name_map_19405 = {
+            "elife-19405-inf1-v1": "elife-19405-inf1",
+            "elife-19405-fig1-v1": "elife-19405-fig1",
+            "elife-19405-v1.pdf": "elife-19405.pdf",
+            "elife-19405-v1.xml": "elife-19405.xml"
+        }
+
+    def tearDown(self):
+        TempDirectory.cleanup_all()
+
+    # input: s3 archive zip file name (name) and date last modified
+    # expected output: file name - highest version file (displayed on -v[number]-) then latest last modified date/time
+    @unpack
+    @data({"input": [{"name": "elife-16747-vor-v1-20160831000000.zip", "last_modified": "2017-05-18T09:04:11.000Z"},
+                    {"name": "elife-16747-vor-v1-20160831132647.zip", "last_modified": "2016-08-31T06:26:56.000Z"}],
+           "expected": "elife-16747-vor-v1-20160831000000.zip"},
+          {"input": [{"name": "elife-16747-vor-v1-20160831000000.zip", "last_modified": "2017-05-18T09:04:11.000Z"},
+                    {"name": "elife-16747-vor-v1-20160831132647.zip", "last_modified": "2016-08-31T06:26:56.000Z"},
+                    {"name": "elife-16747-vor-v2-20160831000000.zip", "last_modified": "2015-01-05T00:20:50.000Z"}],
+           "expected": "elife-16747-vor-v2-20160831000000.zip"}
+          )
+    def test_latest_archive_zip_revision(self, input, expected):
+        output = article_processing.latest_archive_zip_revision("16747", input, "elife", "vor")
+        self.assertEqual(output, expected)
+
+    @unpack
+    @data(
+          {"input": [{"name": "elife-16747-vor-v2-20160831000000.zip", "last_modified": "this_is_junk_for_testing"}],
+           "expected": None}
+          )
+    def test_latest_archive_zip_revision_exception(self, input, expected):
+        output = article_processing.latest_archive_zip_revision("16747", input, "elife", "vor")
+        self.assertRaises(ValueError)
+
+
+    def test_convert_xml(self):
+        xml_file = 'elife-19405-v1.xml'
+        file_name_map = self.file_name_map_19405
+        expected_xml_contains = 'elife-19405.pdf'
+
+        with open('tests/test_data/pmc/' + xml_file, 'rb') as fp:
+            path = self.directory.write(xml_file, fp.read())
+        xml_file_path = os.path.join(self.directory.path, xml_file)
+
+        article_processing.convert_xml(
+            xml_file = xml_file_path,
+            file_name_map = file_name_map)
+
+        with open(xml_file_path, 'rb') as fp:
+            xml_content = fp.read()
+            self.assertTrue(expected_xml_contains in xml_content)
+
+
+    def test_verify_rename_files(self):
+        verified, renamed_list, not_renamed_list = article_processing.verify_rename_files(
+            self.file_name_map_19405)
+        self.assertTrue(verified)
+        self.assertEqual(len(renamed_list), 4)
+        self.assertEqual(len(not_renamed_list), 0)
+
+
+    def test_verify_rename_files_not_renamed(self):
+        verified, renamed_list, not_renamed_list = article_processing.verify_rename_files(
+            {'elife-19405-v1.xml': None})
+        self.assertFalse(verified)
+        self.assertEqual(len(renamed_list), 0)
+        self.assertEqual(len(not_renamed_list), 1)
+
+
+    def test_rename_files_remove_version_number(self):
+        zip_file = 'elife-19405-vor-v1-20160802113816.zip'
+        zip_file_path = 'tests/test_data/pmc/' + zip_file
+        files_dir = 'tmp_dir'
+        output_dir = 'output_didr'
+        # create and set directories
+        self.directory.makedir(output_dir)
+        self.directory.makedir(files_dir)
+        files_dir_path = os.path.join(self.directory.path, files_dir)
+        output_dir_path = os.path.join(self.directory.path, output_dir)
+
+        # unzip the test data
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_file:
+            zip_file.extractall(files_dir_path)
+
+        # now can run the function we are testing
+        article_processing.rename_files_remove_version_number(files_dir_path, output_dir_path)
+
+
+    @unpack
+    @data(
+        ('elife', '1', '7', None, 'elife-01-00007.zip'),
+        ('elife', '1', '7', '1', 'elife-01-00007.r1.zip')
+    )
+    def test_new_pmc_zip_filename(self, journal, volume, fid, revision, expected):
+        self.assertEqual(article_processing.new_pmc_zip_filename(
+            journal, volume, fid, revision), expected)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
