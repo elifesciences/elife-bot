e8f1a85c5fef1b8111917b2809e6a092aaf4e91d
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-14 15:25:27 +0100

    Removed activity ResizeImages

--- register.py
+++ register.py
@@ -90,7 +90,6 @@ def start(ENV="dev"):
     activity_names.append("RewriteEIF")
     activity_names.append("DepositAssets")
     activity_names.append("ApprovePublication")
-    activity_names.append("ResizeImages")
     activity_names.append("PreparePostEIF")
     activity_names.append("Sum")
     activity_names.append("S3Monitor")
