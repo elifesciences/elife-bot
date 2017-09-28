df18e77419e1bc6f239b0f3d527a3d40a4b97c2e
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-28 15:42:57 +0100

    Added two other test scenarios

--- tests/activity/test_activity_update_repository.py
+++ tests/activity/test_activity_update_repository.py
@@ -11,6 +11,20 @@ from classes_mock import FakeStorageContext, FakeLogger, FakeLaxProvider
 @patch('dashboard_queue.send_message')
 class TestUpdateRepository(unittest.TestCase):
 
+
+    def test_happy_path(self, dashboard_queue, mock_storage_context, provider):
+        a = activity_UpdateRepository(settings_mock, FakeLogger())
+        mock_storage_context.return_value = FakeStorageContext()
+        provider.lax_provider = FakeLaxProvider
+        a.update_github = MagicMock()
+
+        result = a.do_activity({
+            'article_id': '12345',
+            'version': 1,
+            'run': '123abc',
+        })
+        self.assertTrue(result)
+
     def test_ssl_timeout_error_leads_to_a_retry(self, dashboard_queue, mock_storage_context, provider):
         a = activity_UpdateRepository(settings_mock, FakeLogger())
         mock_storage_context.return_value = FakeStorageContext()
@@ -26,3 +40,17 @@ class TestUpdateRepository(unittest.TestCase):
         })
         self.assertEqual(result, 'ActivityTemporaryFailure')
 
+    def test_generic_errors_we_dont_know_how_to_treat_lead_to_permanent_failures(self, dashboard_queue, mock_storage_context, provider):
+        a = activity_UpdateRepository(settings_mock, FakeLogger())
+        mock_storage_context.return_value = FakeStorageContext()
+        provider.lax_provider = FakeLaxProvider
+        a.update_github = MagicMock()
+
+        a.update_github.side_effect = RuntimeError('One of the cross beams has gone out askew...')
+
+        result = a.do_activity({
+            'article_id': '12345',
+            'version': 1,
+            'run': '123abc',
+        })
+        self.assertEqual(result, 'ActivityPermanentFailure')
