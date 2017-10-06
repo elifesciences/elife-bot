9150d37c83633ab202fbb9700cf3b8b48c2a5c64
Graham Nott <gnott@starglobal.ca>
2017-10-06 12:56:49 -0700

    Padding id value returned by Lax in the lax_response_adapter.py with a test case.

--- tests/test_lax_response_adapter.py
+++ tests/test_lax_response_adapter.py
@@ -29,6 +29,31 @@ workflow_message_expected = {'workflow_data':
                                   'version': u'1'},
                              'workflow_name': 'PostPerfectPublication'}
 
+fake_token_269 = json.dumps({u'status': u'vor',
+                         u'expanded_folder': u'00269.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+                         u'eif_location': u'00269.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148/elife-00269-v1.json',
+                         u'version': u'1',
+                         u'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148'})
+
+fake_lax_message_269 = json.dumps({"status": "published",
+                               "requested-action": "publish",
+                               "datetime": "2013-03-26T00:00:00+00:00",
+                               "token": base64.encodestring(fake_token_269),
+                               "id": "269"})
+
+workflow_message_expected_269 = {'workflow_data':
+                                 {'article_id': u'00269',
+                                  'eif_location': u'00269.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148/elife-00269-v1.json',
+                                  'expanded_folder': u'00269.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+                                  'message': None,
+                                  'requested_action': u'publish',
+                                  'result': u'published',
+                                  'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+                                  'status': u'vor',
+                                  'update_date': '2013-03-26T00:00:00Z',
+                                  'version': u'1'},
+                             'workflow_name': 'PostPerfectPublication'}
+
 class TestLaxResponseAdapter(unittest.TestCase):
     def setUp(self):
         settings = Mock()
@@ -40,6 +65,10 @@ class TestLaxResponseAdapter(unittest.TestCase):
         self.assertDictEqual.__self__.maxDiff = None
         self.assertDictEqual(expected_workflow_starter_message, workflow_message_expected)
 
+    def test_parse_message_269(self):
+        expected_workflow_starter_message = self.laxresponseadapter.parse_message(fake_lax_message_269)
+        self.assertDictEqual.__self__.maxDiff = None
+        self.assertDictEqual(expected_workflow_starter_message, workflow_message_expected_269)
 
 if __name__ == '__main__':
     unittest.main()
