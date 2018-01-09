a7101785b07cf85a04fd6565a1c5971aa407f708
Graham Nott <gnott@starglobal.ca>
2018-01-09 09:43:31 -0800

    Override BOTO_CONFIG in travis-ci builds.

--- travis-install.sh
+++ travis-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e # everything must succeed.
+export BOTO_CONFIG=/dev/null
 pip install -r requirements.txt
 git clone https://github.com/elifesciences/elife-poa-xml-generation.git ../elife-poa-xml-generation
 cp ../elife-poa-xml-generation/example-settings.py ../elife-poa-xml-generation/settings.py
