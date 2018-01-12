b711725c35c215ce4db1b5a19852b885c70b825e
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-12 12:16:44 +0000

    Updated test of RewriteEIF to provide session data

--- tests/activity/test_activity_rewrite_eif.py
+++ tests/activity/test_activity_rewrite_eif.py
@@ -5,6 +5,7 @@ import test_activity_data as data
 from mock import mock, patch
 from classes_mock import FakeKey
 from classes_mock import FakeS3Connection
+from classes_mock import FakeSession
 import classes_mock
 from testfixtures import TempDirectory
 import json
@@ -20,12 +21,14 @@ class tests_RewriteEIF(unittest.TestCase):
         TempDirectory.cleanup_all()
 
     @patch('activity.activity_RewriteEIF.eif_provider.Key')
+    @patch('activity.activity_RewriteEIF.get_session')
     @patch('activity.activity_RewriteEIF.S3Connection')
     @patch.object(activity_RewriteEIF, 'emit_monitor_event')
     @patch.object(activity_RewriteEIF, 'set_monitor_property')
-    def test_activity(self, mock_set_monitor_property, mock_emit_monitor_event, fake_s3_mock, fake_key_mock):
+    def test_activity(self, mock_set_monitor_property, mock_emit_monitor_event, fake_s3_mock, fake_session, fake_key_mock):
         directory = TempDirectory()
 
+        fake_session.return_value = FakeSession(data.session_example)
         fake_key_mock.return_value = FakeKey(directory, data.bucket_dest_file_name,
                                              data.RewriteEIF_json_input_string)
         fake_s3_mock.return_value = FakeS3Connection()
