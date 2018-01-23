aa58927d5dff6c204bf6c1dcce4f14b6f73407c3
jennifer <jenniferstrej@gmail.com>
2018-01-23 10:57:06 +0000

    fixes tests and raise exception deliberately

--- tests/activity/test_activity_ready_to_publish.py
+++ tests/activity/test_activity_ready_to_publish.py
@@ -28,11 +28,13 @@ class TestReadyToPublish(unittest.TestCase):
         result = self.readytopublish.do_activity(test_data)
         self.assertEqual(result, self.readytopublish.ACTIVITY_SUCCESS)
 
+    @patch.object(activity_ReadyToPublish, 'prepare_ready_to_publish_message')
     @patch('activity.activity_ReadyToPublish.get_session')
     @patch.object(activity_ReadyToPublish, 'emit_monitor_event')
-    def test_ready_to_publish_error(self, fake_emit_monitor, fake_session):
+    def test_ready_to_publish_error(self, fake_emit_monitor, fake_session, fake_prepare):
         fake_session.return_value = FakeSession(session_example)
         fake_logger = FakeLogger()
+        fake_prepare.side_effect = Exception("An error occurred")
         self.activity_ReadyToPublish_with_log = activity_ReadyToPublish(settings_mock, fake_logger, None, None, None)
 
 
