59800002a542bb31fd5800edda425639ff8c84ae
Giorgio Sironi <g.sironi@elifesciences.org>
2018-05-24 15:20:33 +0100

    Implement Fastly provider with requests

--- provider/fastly_provider.py
+++ provider/fastly_provider.py
@@ -1,3 +1,13 @@
+import requests
 
-def purge(*args):
-    pass
+def purge(article_id, settings):
+    api = fastly.API()
+    surrogate_key = 'articles/%05d' % article_id
+    url = "https://api.fastly.com/service/%s/purge/%s" % (settings.fastly_service_id, surrogate_key)
+
+    response = requests.post(url, headers={
+        'Accept': 'application/json',
+        'Fastly-Key': settings.fastly_api_key
+    })
+    response.raise_for_status()
+    return response
---
+++ tests/provider/test_fastly_provider.py
@@ -0,0 +1,21 @@
+import unittest
+from mock import mock, patch
+from provider import fastly_provider
+from tests import settings_mock
+from requests.models import Response
+from requests.exceptions import HTTPError
+
+class TestFastlyProvider(unittest.TestCase):
+    @patch('requests.post')
+    def test_purge(self, post_mock):
+        response = Response()
+        response.status_code = 200
+        post_mock.return_value = response
+        fastly_provider.purge(10627, settings_mock)
+
+    @patch('requests.post')
+    def test_purge_failure(self, post_mock):
+        response = Response()
+        response.status_code = 500
+        post_mock.return_value = response
+        self.assertRaises(HTTPError, lambda: fastly_provider.purge(10627, settings_mock))
