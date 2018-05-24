a303dcff50d71f4179a6e6c59be681c3c1d0a111
Giorgio Sironi <g.sironi@elifesciences.org>
2018-05-24 17:17:40 +0100

    Fix article_id type in invalidation
    Overlooked that it is a string, not a number

--- tests/provider/test_fastly_provider.py
+++ tests/provider/test_fastly_provider.py
@@ -11,11 +11,11 @@ class TestFastlyProvider(unittest.TestCase):
         response = Response()
         response.status_code = 200
         post_mock.return_value = response
-        fastly_provider.purge(10627, settings_mock)
+        fastly_provider.purge('10627', settings_mock)
 
     @patch('requests.post')
     def test_purge_failure(self, post_mock):
         response = Response()
         response.status_code = 500
         post_mock.return_value = response
-        self.assertRaises(HTTPError, lambda: fastly_provider.purge(10627, settings_mock))
+        self.assertRaises(HTTPError, lambda: fastly_provider.purge('10627', settings_mock))
