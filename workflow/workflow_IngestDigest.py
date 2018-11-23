52ec887317df62f0a2fdc4415b108a44aa26cc25
Graham Nott <gnott@starglobal.ca>
2018-11-22 20:57:18 -0800

    Add PostDigestJATS activity to the IngestDigest workflow.

--- workflow/workflow_IngestDigest.py
+++ workflow/workflow_IngestDigest.py
@@ -89,6 +89,17 @@ class workflow_IngestDigest(workflow.workflow):
                         "schedule_to_start_timeout": 300,
                         "start_to_close_timeout": 300
                     },
+                    {
+                        "activity_type": "PostDigestJATS",
+                        "activity_id": "PostDigestJATS",
+                        "version": "1",
+                        "input": data,
+                        "control": None,
+                        "heartbeat_timeout": 300,
+                        "schedule_to_close_timeout": 300,
+                        "schedule_to_start_timeout": 300,
+                        "start_to_close_timeout": 300
+                    },
                 ],
 
             "finish":
