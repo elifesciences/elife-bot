abb8e95059fcb23372c7028ed26eeea56bd803a6
Graham Nott <gnott@starglobal.ca>
2019-01-03 20:03:01 -0800

    Test for do_activity() of PubRouterDeposit.

--- tests/activity/test_activity_pub_router_deposit.py
+++ tests/activity/test_activity_pub_router_deposit.py
@@ -1,12 +1,53 @@
 import unittest
-from activity.activity_PubRouterDeposit import activity_PubRouterDeposit
-import tests.activity.settings_mock as settings_mock
+import os
+import shutil
+from mock import patch
 from ddt import ddt, data, unpack
+import provider.s3lib as s3lib
+from activity.activity_PubRouterDeposit import activity_PubRouterDeposit
+import tests.test_data as test_case_data
+import tests.activity.settings_mock as settings_mock
+from tests.activity.classes_mock import FakeLogger
+
+
+def download_files(filenames, to_dir):
+    copied_filenames = []
+    for filename in filenames:
+        source_doc = "tests/test_data/" + filename
+        dest_doc = os.path.join(to_dir, filename)
+        try:
+            shutil.copy(source_doc, dest_doc)
+            copied_filenames.append(dest_doc)
+        except IOError:
+            pass
+    return copied_filenames
+
 
 @ddt
 class TestPubRouterDeposit(unittest.TestCase):
     def setUp(self):
-        self.pubrouterdeposit = activity_PubRouterDeposit(settings_mock, None, None, None, None)
+        self.pubrouterdeposit = activity_PubRouterDeposit(
+            settings_mock, FakeLogger(), None, None, None)
+
+    @patch('provider.lax_provider.article_versions')
+    @patch.object(activity_PubRouterDeposit, 'start_ftp_article_workflow')
+    @patch.object(activity_PubRouterDeposit, 'does_source_zip_exist_from_s3')
+    @patch.object(activity_PubRouterDeposit, 'download_files_from_s3_outbox')
+    @patch.object(s3lib, 'get_s3_keys_from_bucket')
+    def test_do_activity(self, fake_get_s3_keys, fake_download, fake_zip_exists,
+                         fake_start, fake_article_versions):
+        activity_data = {
+            "data": {
+                "workflow": "HEFCE"
+            }
+        }
+        fake_download.return_value = download_files(
+            ["elife00013.xml", "elife09169.xml"], self.pubrouterdeposit.get_tmp_dir())
+        fake_zip_exists.return_value = True
+        fake_start.return_value = True
+        fake_article_versions.return_value = 200, test_case_data.lax_article_versions_response_data
+        result = self.pubrouterdeposit.do_activity(activity_data)
+        self.assertTrue(result)
 
     # input: s3 archive zip file name (name) and date last modified
     # expected output: file name - highest version file (displayed on -v[number]-) then latest last modified date/time
