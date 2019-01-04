f7006b58b6acc917660feaf643fa416f35649ff8
Graham Nott <gnott@starglobal.ca>
2019-01-03 20:09:53 -0800

    Linting for long lines and variable name in PubRouterDeposit test.

--- tests/activity/test_activity_pub_router_deposit.py
+++ tests/activity/test_activity_pub_router_deposit.py
@@ -50,18 +50,43 @@ class TestPubRouterDeposit(unittest.TestCase):
         self.assertTrue(result)
 
     # input: s3 archive zip file name (name) and date last modified
-    # expected output: file name - highest version file (displayed on -v[number]-) then latest last modified date/time
+    # expected output: file name - highest version file (displayed on -v[number]-)
+    # then latest last modified date/time
     @unpack
-    @data({"input": [{"name": "elife-16747-vor-v1-20160831000000.zip", "last_modified": "2017-05-18T09:04:11.000Z"},
-                    {"name": "elife-16747-vor-v1-20160831132647.zip", "last_modified": "2016-08-31T06:26:56.000Z"}],
-           "expected": "elife-16747-vor-v1-20160831000000.zip"},
-          {"input": [{"name": "elife-16747-vor-v1-20160831000000.zip", "last_modified": "2017-05-18T09:04:11.000Z"},
-                    {"name": "elife-16747-vor-v1-20160831132647.zip", "last_modified": "2016-08-31T06:26:56.000Z"},
-                    {"name": "elife-16747-vor-v2-20160831000000.zip", "last_modified": "2015-01-05T00:20:50.000Z"}],
-           "expected": "elife-16747-vor-v2-20160831000000.zip"}
-          )
-    def test_latest_archive_zip_revision(self, input, expected):
-        output = self.pubrouterdeposit.latest_archive_zip_revision("16747", input, "elife", "vor")
+    @data(
+        {
+            "s3_keys": [
+                {
+                    "name": "elife-16747-vor-v1-20160831000000.zip",
+                    "last_modified": "2017-05-18T09:04:11.000Z"
+                },
+                {
+                    "name": "elife-16747-vor-v1-20160831132647.zip",
+                    "last_modified": "2016-08-31T06:26:56.000Z"
+                }
+            ],
+            "expected": "elife-16747-vor-v1-20160831000000.zip"
+            },
+        {
+            "s3_keys": [
+                {
+                    "name": "elife-16747-vor-v1-20160831000000.zip",
+                    "last_modified": "2017-05-18T09:04:11.000Z"
+                },
+                {
+                    "name": "elife-16747-vor-v1-20160831132647.zip",
+                    "last_modified": "2016-08-31T06:26:56.000Z"
+                },
+                {
+                    "name": "elife-16747-vor-v2-20160831000000.zip",
+                    "last_modified": "2015-01-05T00:20:50.000Z"
+                }
+            ],
+            "expected": "elife-16747-vor-v2-20160831000000.zip"
+            }
+        )
+    def test_latest_archive_zip_revision(self, s3_keys, expected):
+        output = self.pubrouterdeposit.latest_archive_zip_revision("16747", s3_keys, "elife", "vor")
         self.assertEqual(output, expected)
 
 
