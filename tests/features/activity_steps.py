433925fa4223fd307b0791a6f154b98d395692fa
Graham Nott <gnott@starglobal.ca>
2019-01-07 16:08:20 -0800

    Remove activity lettuce tests, are already covered.

--- tests/features/activity_steps.py
+++ tests/features/activity_steps.py
@@ -8,27 +8,6 @@ def i_convert_the_document_name_from_path_to_jpg_filename_using_the_activity_obj
     assert world.document_name_from_path is not None, \
         "Got document_name_from_path %s" % world.document_name_from_path
 
-@step('I can get a domain from the activity')
-def get_domain_from_activity_object(step):
-    assert world.activity_object.domain is not None, \
-        "Got domain %s" % world.activity_object.domain
-    
-@step('I can get a task_list from the activity')
-def get_task_list_from_activity_object(step):
-    assert world.activity_object.task_list is not None, \
-        "Got task_list %s" % world.activity_object.task_list
-    
-@step('I get the activity name (\S+)')
-def get_activity_name(step, name):
-    assert world.activity_object.name == name, \
-        "Got name %s" % world.activity_object.name
-
-@step('I get a result from the activity')
-def get_result_from_activity_object(step):
-    world.result = world.activity_object.do_activity(world.json)
-    assert world.result is not None, \
-        "Got result %s" % world.result
-
 @step('I have the document name (\S+)')
 def have_the_document_name(step, document_name):
     world.document_name = document_name
