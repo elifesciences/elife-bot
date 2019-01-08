2e3e97a06662a49807f4710d2557af3350360004
Graham Nott <gnott@starglobal.ca>
2019-01-08 12:45:59 -0800

    Remove remaining lettuce test files, runners and requirements.

--- project_tests.sh
+++ project_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 source venv/bin/activate
-cd tests/
-for i in `ls features/*.feature`; do echo $i; lettuce $i; done
-cd -
 rm -f build/junit.xml
 ./lint.sh
 bash ./travis-test.sh
