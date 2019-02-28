2acdcd263836fce857a7240a527f5abb103bd717
Graham Nott <gnott@starglobal.ca>
2019-02-27 16:46:49 -0800

    Basic linting of test_lax_response_adapter.py

--- tests/test_lax_response_adapter.py
+++ tests/test_lax_response_adapter.py
@@ -1,60 +1,67 @@
 import unittest
+import json
 from lax_response_adapter import LaxResponseAdapter
 from mock import Mock
-import json
 from provider.utils import base64_encode_string
 
-fake_token = json.dumps({u'status': u'vor',
-                         u'expanded_folder': u'837411455.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
-                         u'version': u'1',
-                         u'force': False,
-                         u'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148'})
+FAKE_TOKEN = json.dumps({
+    u'status': u'vor',
+    u'expanded_folder': u'837411455.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+    u'version': u'1',
+    u'force': False,
+    u'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148'})
 
-fake_lax_message = json.dumps({"status": "published",
-                               "requested-action": "publish",
-                               "datetime": "2013-03-26T00:00:00+00:00",
-                               "token": base64_encode_string(fake_token),
-                               "id": "837411455"})
+FAKE_LAX_MESSAGE = json.dumps({
+    "status": "published",
+    "requested-action": "publish",
+    "datetime": "2013-03-26T00:00:00+00:00",
+    "token": base64_encode_string(FAKE_TOKEN),
+    "id": "837411455"})
 
-workflow_message_expected = {'workflow_data':
-                                 {'article_id': u'837411455',
-                                  'expanded_folder': u'837411455.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
-                                  'message': None,
-                                  'requested_action': u'publish',
-                                  'force': False,
-                                  'result': u'published',
-                                  'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148',
-                                  'status': u'vor',
-                                  'update_date': '2013-03-26T00:00:00Z',
-                                  'version': u'1',
-                                  'run_type': None},
-                             'workflow_name': 'PostPerfectPublication'}
+WORKFLOW_MESSAGE_EXPECTED = {
+    'workflow_data': {
+        'article_id': u'837411455',
+        'expanded_folder': u'837411455.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+        'message': None,
+        'requested_action': u'publish',
+        'force': False,
+        'result': u'published',
+        'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+        'status': u'vor',
+        'update_date': '2013-03-26T00:00:00Z',
+        'version': u'1',
+        'run_type': None},
+    'workflow_name': 'PostPerfectPublication'}
 
-fake_token_269 = json.dumps({u'status': u'vor',
-                         u'expanded_folder': u'00269.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
-                         u'version': u'1',
-                         u'force': False,
-                         u'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148'})
+FAKE_TOKEN_269 = json.dumps({
+    u'status': u'vor',
+    u'expanded_folder': u'00269.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+    u'version': u'1',
+    u'force': False,
+    u'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148'})
 
-fake_lax_message_269 = json.dumps({"status": "published",
-                               "requested-action": "publish",
-                               "datetime": "2013-03-26T00:00:00+00:00",
-                               "token": base64_encode_string(fake_token_269),
-                               "id": "269"})
+FAKE_LAX_MESSAGE_269 = json.dumps({
+    "status": "published",
+    "requested-action": "publish",
+    "datetime": "2013-03-26T00:00:00+00:00",
+    "token": base64_encode_string(FAKE_TOKEN_269),
+    "id": "269"})
 
-workflow_message_expected_269 = {'workflow_data':
-                                 {'article_id': u'00269',
-                                  'expanded_folder': u'00269.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
-                                  'message': None,
-                                  'requested_action': u'publish',
-                                  'force': False,
-                                  'result': u'published',
-                                  'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148',
-                                  'status': u'vor',
-                                  'update_date': '2013-03-26T00:00:00Z',
-                                  'version': u'1',
-                                  'run_type': None},
-                             'workflow_name': 'PostPerfectPublication'}
+WORKFLOW_MESSAGE_EXPECTED_269 = {
+    'workflow_data': {
+        'article_id': u'00269',
+        'expanded_folder': u'00269.1/a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+        'message': None,
+        'requested_action': u'publish',
+        'force': False,
+        'result': u'published',
+        'run': u'a8bb05df-2df9-4fce-8f9f-219aca0b0148',
+        'status': u'vor',
+        'update_date': '2013-03-26T00:00:00Z',
+        'version': u'1',
+        'run_type': None},
+    'workflow_name': 'PostPerfectPublication'}
+
 
 class TestLaxResponseAdapter(unittest.TestCase):
     def setUp(self):
@@ -63,14 +70,17 @@ class TestLaxResponseAdapter(unittest.TestCase):
         self.laxresponseadapter = LaxResponseAdapter(settings, self.logger)
 
     def test_parse_message(self):
-        expected_workflow_starter_message = self.laxresponseadapter.parse_message(fake_lax_message)
+        expected_workflow_starter_message = self.laxresponseadapter.parse_message(
+            FAKE_LAX_MESSAGE)
         self.assertDictEqual.__self__.maxDiff = None
-        self.assertDictEqual(expected_workflow_starter_message, workflow_message_expected)
+        self.assertDictEqual(expected_workflow_starter_message, WORKFLOW_MESSAGE_EXPECTED)
 
     def test_parse_message_269(self):
-        expected_workflow_starter_message = self.laxresponseadapter.parse_message(fake_lax_message_269)
+        expected_workflow_starter_message = self.laxresponseadapter.parse_message(
+            FAKE_LAX_MESSAGE_269)
         self.assertDictEqual.__self__.maxDiff = None
-        self.assertDictEqual(expected_workflow_starter_message, workflow_message_expected_269)
+        self.assertDictEqual(expected_workflow_starter_message, WORKFLOW_MESSAGE_EXPECTED_269)
+
 
 if __name__ == '__main__':
     unittest.main()
