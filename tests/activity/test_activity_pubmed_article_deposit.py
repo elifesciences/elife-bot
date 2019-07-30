05e660ceb3d1c185fc7e9d59f6342112795e7795
Graham Nott <gnott@starglobal.ca>
2019-07-30 06:47:03 -0700

    Remove test method not used.

--- tests/activity/test_activity_pubmed_article_deposit.py
+++ tests/activity/test_activity_pubmed_article_deposit.py
@@ -25,10 +25,6 @@ class TestPubmedArticleDeposit(unittest.TestCase):
         helpers.delete_files_in_folder(activity_test_data.ExpandArticle_files_dest_folder,
                                        filter_out=['.gitkeep'])
 
-    def input_dir(self):
-        "return the staging dir name for the activity"
-        return os.path.join(self.activity.get_tmp_dir(), self.activity.INPUT_DIR)
-
     def tmp_dir(self):
         "return the tmp dir name for the activity"
         return self.activity.directories.get("TMP_DIR")
