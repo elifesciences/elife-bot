7dfc26608eb26defc98c4e2a4f28e7b617dd3343
Luke <lsh-0@users.noreply.github.com>
2019-08-30 12:20:33 +0930

    tweaks to Graham's python3 branch. downgrades python-docx to a version that installs on python 3.5.2 moved mkvenv.sh logic back into mkvenv.sh script. changed travis execution environment to 16.04 from 14.04

--- install.sh
+++ install.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 set -e
 
-python=''
-pybinlist=("python3.6" "python3.5" "python3.4")
-
-for pybin in ${pybinlist[*]}; do
-    which "$pybin" &> /dev/null || continue
-    python=$pybin
-    break
-done
-
-if [ -z "$python" ]; then
-    echo "no usable python found, exiting"
-    exit 1
-fi
-
-echo "Found Python: $($python --version)"
-
-if [ "$ENVIRONMENT_NAME" == "ci" ]; then
-    rm -rf venv/
-fi
-
-if [ ! -d venv ]; then
-    # build venv if one doesn't exist
-    $python -m venv venv
-fi
+. mkvenv.sh
 
 echo "Virtualenv Python: $(venv/bin/python --version)"
 
@@ -37,7 +14,7 @@ find starter/ -maxdepth 1 -name '*.pyc' -delete
 find S3utility/ -maxdepth 1 -name '*.pyc' -delete
 
 source venv/bin/activate
-pip install --upgrade pip
+
 grep "git+" requirements.txt > source-requirements.txt
 #pip uninstall -r source-requirements.txt -y
 pip install --ignore-installed -r source-requirements.txt
---
+++ mkvenv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+python=''
+pybinlist=("python3.6" "python3.5" "python3.4")
+
+for pybin in ${pybinlist[*]}; do
+    which "$pybin" &> /dev/null || continue
+    python=$pybin
+    break
+done
+
+if [ -z "$python" ]; then
+    echo "no usable python found, exiting"
+    exit 1
+fi
+
+if [ ! -e "venv/bin/$python" ]; then
+    echo "could not find venv/bin/$python, recreating venv"
+    rm -rf venv
+    $python -m venv venv
+else
+    echo "using $python"
+fi
