602433032d1a8d2d10af91cd784f79b47549e197
Graham Nott <gnott@starglobal.ca>
2019-09-11 07:58:51 -0700

    VerifyGlencoe again just before IngestToLax.

--- workflow/workflow_SilentCorrectionsIngest.py
+++ workflow/workflow_SilentCorrectionsIngest.py
@@ -190,6 +190,17 @@ class workflow_SilentCorrectionsIngest(Workflow):
                         "start_to_close_timeout": 60 * 15
                     },
                     {
+                        "activity_type": "VerifyGlencoe",
+                        "activity_id": "VerifyGlencoe",
+                        "version": "1",
+                        "input": data,
+                        "control": None,
+                        "heartbeat_timeout": 60 * 15,
+                        "schedule_to_close_timeout": 60 * 15,
+                        "schedule_to_start_timeout": 300,
+                        "start_to_close_timeout": 60 * 15
+                    },
+                    {
                         "activity_type": "IngestToLax",
                         "activity_id": "IngestToLax",
                         "version": "1",
