eb64a083af0c244f1621aaf0e0f7176ae2da8e7a
Graham Nott <gnott@starglobal.ca>
2019-10-04 16:33:05 -0700

    Starter for DepositCrossrefPeerReview workflow.

---
+++ starter/starter_DepositCrossrefPeerReview.py
@@ -0,0 +1,60 @@
+import boto.swf
+import log
+import json
+import random
+
+from optparse import OptionParser
+
+
+class starter_DepositCrossrefPeerReview():
+
+    def start(self, settings):
+        # Log
+        identity = "starter_%s" % int(random.random() * 1000)
+        logFile = "starter.log"
+        logger = log.logger(logFile, settings.setLevel, identity)
+
+        # Simple connect
+        conn = boto.swf.layer1.Layer1(settings.aws_access_key_id, settings.aws_secret_access_key)
+
+        # Start a workflow execution
+        workflow_id = "DepositCrossrefPeerReview"
+        workflow_name = "DepositCrossrefPeerReview"
+        workflow_version = "1"
+        child_policy = None
+        execution_start_to_close_timeout = None
+        input = None
+
+        try:
+            response = conn.start_workflow_execution(settings.domain, workflow_id, workflow_name,
+                                                     workflow_version, settings.default_task_list,
+                                                     child_policy, execution_start_to_close_timeout,
+                                                     input)
+
+            logger.info('got response: \n%s' % json.dumps(response, sort_keys=True, indent=4))
+
+        except boto.swf.exceptions.SWFWorkflowExecutionAlreadyStartedError:
+            # There is already a running workflow with that ID, cannot start another
+            message = ('SWFWorkflowExecutionAlreadyStartedError: There is already ' +
+                       'a running workflow with ID %s' % workflow_id)
+            print(message)
+            logger.info(message)
+
+
+if __name__ == "__main__":
+
+    # Add options
+    parser = OptionParser()
+    parser.add_option("-e", "--env", default="dev", action="store", type="string", dest="env",
+                      help="set the environment to run, either dev or live")
+
+    (options, args) = parser.parse_args()
+    if options.env:
+        ENV = options.env
+
+    import settings as settingsLib
+    settings = settingsLib.get_settings(ENV)
+
+    o = starter_DepositCrossrefPeerReview()
+
+    o.start(settings=settings)
