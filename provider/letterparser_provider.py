6744ef25474335eacf4ae4fc7814756caa784962
Graham Nott <gnott@starglobal.ca>
2019-11-07 16:39:18 -0800

    Simple call to pandoc via Docker in letterparser.

---
+++ provider/letterparser_provider.py
@@ -0,0 +1,16 @@
+"functions shared by letterparser related activities"
+
+import os
+import docker
+from letterparser import parse
+import log
+
+IDENTITY = "process_%s" % os.getpid()
+LOGGER = log.logger("letterparser_provider.log", 'INFO', IDENTITY, loggerName=__name__)
+
+
+def docker_pandoc_output(file_name, config):
+    try:
+        return parse.docker_pandoc_output(file_name, config)
+    except docker.errors.APIError:
+        LOGGER.info('Error connecting to docker')
---
+++ tests/provider/test_letterparser_provider.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+import unittest
+import provider.letterparser_provider as letterparser_provider
+
+
+class TestLetterParserProvider(unittest.TestCase):
+
+    def test_docker_pandoc_output(self):
+        """test calling pandoc via Docker"""
+        file_name = 'tests/fixtures/letterparser/sections.docx'
+        # fake config for specifying Docker image
+        config = {'docker_image': 'knsit/pandoc:v2.5'}
+        expected = (
+            '<p><bold>Preamble<break /></bold></p>\n'
+            '<p>Preamble ....</p>\n'
+            '<p><bold>Decision letter</bold></p>\n'
+            '<p>Decision letter ....</p>\n'
+            '<p><bold>Author response</bold></p>\n'
+            '<p>Author response ....</p>\n')
+
+        output = letterparser_provider.docker_pandoc_output(file_name, config)
+        self.assertEqual(output, expected, 'Docker pandoc output not equal, is Docker running?')
