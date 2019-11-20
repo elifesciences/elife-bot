3d42e8c5059b2085ec69ac30ef292059df498dfe
Graham Nott <gnott@starglobal.ca>
2019-11-19 16:33:16 -0800

    Test for do_activity exception in schedule peer review.

--- tests/activity/test_activity_schedule_crossref_peer_review.py
+++ tests/activity/test_activity_schedule_crossref_peer_review.py
@@ -1,4 +1,5 @@
 import unittest
+import copy
 from mock import mock, patch
 import activity.activity_ScheduleCrossrefPeerReview as activity_module
 from activity.activity_ScheduleCrossrefPeerReview import (
@@ -47,7 +48,7 @@ class TestScheduleCrossrefPeerReview(unittest.TestCase):
         expected_result = True
         fake_set_property.return_value = True
         fake_emit_monitor.return_value = True
-        session_dict = activity_test_data.session_example
+        session_dict = copy.copy(activity_test_data.session_example)
         session_dict['run_type'] = 'silent-correction'
         fake_session_mock.return_value = FakeSession(session_dict)
         fake_highest_version.return_value = 2
@@ -76,6 +77,26 @@ class TestScheduleCrossrefPeerReview(unittest.TestCase):
         self.assertEqual(self.activity.logger.loginfo, (
             'ScheduleCrossrefPeerReview finds version 1 of 00353 has no sub-article'
             ' for peer review depositing'))
+
+    @patch.object(activity_object, 'copy_article_xml_to_outbox')
+    @patch('provider.lax_provider.article_highest_version')
+    @patch.object(activity_module, 'get_session')
+    @patch.object(activity_object, 'xml_sub_article_exists')
+    @patch.object(activity_object, 'emit_monitor_event')
+    @patch.object(activity_object, 'set_monitor_property')
+    def test_do_activity_exception(self, fake_set_property, fake_emit_monitor,
+                                   fake_sub_article_exists, fake_session_mock,
+                                   fake_highest_version, fake_copy_article):
+        expected_result = False
+        fake_sub_article_exists.return_value = True
+        fake_set_property.return_value = True
+        fake_emit_monitor.return_value = True
+        fake_copy_article.side_effect = Exception("Something went wrong!")
+        fake_session_mock.return_value = FakeSession(activity_test_data.session_example)
+        fake_highest_version.return_value = 1
+        # do the activity
+        result = self.activity.do_activity(activity_test_data.ExpandArticle_data)
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
