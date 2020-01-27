c436c0f415d9c5792d094c7ecce88e4778f2f244
Graham Nott <gnott@starglobal.ca>
2020-01-27 08:55:56 -0800

    self.logger will always be present, remove conditional.

--- activity/activity_GenerateDecisionLetterJATS.py
+++ activity/activity_GenerateDecisionLetterJATS.py
@@ -47,8 +47,7 @@ class activity_GenerateDecisionLetterJATS(Activity):
         """
         Activity, do the work
         """
-        if self.logger:
-            self.logger.info('data: %s' % json.dumps(data, sort_keys=True, indent=4))
+        self.logger.info('data: %s' % json.dumps(data, sort_keys=True, indent=4))
 
         self.make_activity_directories()
 
