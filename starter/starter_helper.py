de3bdf3e1026e1523200473d206d28f49667a643
Graham Nott <gnott@starglobal.ca>
2020-01-31 10:47:25 -0800

    Linting indentation in starter_helper.py

--- starter/starter_helper.py
+++ starter/starter_helper.py
@@ -2,32 +2,34 @@ import os
 import json
 import log
 
+
 class NullRequiredDataException(Exception):
     def __init__(self, message):
         self.message = message
 
+
 def get_starter_identity(name):
-        return "starter_" + name + "." + str(os.getpid())
+    return "starter_" + name + "." + str(os.getpid())
 
 
 def get_starter_logger(set_level, identity, log_file="starter.log"):
-        return log.logger(log_file, set_level, identity)
+    return log.logger(log_file, set_level, identity)
 
 
 def set_workflow_information(name, workflow_version, child_policy, data, workflow_id_part,
                              extra="", start_to_close_timeout=str(60 * 30)):
-        workflow_id = "%s_%s" % (name, workflow_id_part)
-        if extra:
-            workflow_id = workflow_id + (".%s" % extra)
-        workflow_name = name
-        workflow_version = workflow_version
-        child_policy = child_policy
-        execution_start_to_close_timeout = start_to_close_timeout
-        workflow_input = json.dumps(data, default=lambda ob: None)
+    workflow_id = "%s_%s" % (name, workflow_id_part)
+    if extra:
+        workflow_id = workflow_id + (".%s" % extra)
+    workflow_name = name
+    workflow_version = workflow_version
+    child_policy = child_policy
+    execution_start_to_close_timeout = start_to_close_timeout
+    workflow_input = json.dumps(data, default=lambda ob: None)
 
-        return workflow_id, \
-               workflow_name, \
-               workflow_version, \
-               child_policy, \
-               execution_start_to_close_timeout, \
-               workflow_input
+    return workflow_id, \
+        workflow_name, \
+        workflow_version, \
+        child_policy, \
+        execution_start_to_close_timeout, \
+        workflow_input
