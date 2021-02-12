ddbfcc62c3e2b81bba1ad52cf29b9bd71a73a649
Graham Nott <gnott@starglobal.ca>
2021-02-12 15:06:20 -0800

    Refactor DownstreamStart test data.

--- tests/activity/test_activity_downstream_start.py
+++ tests/activity/test_activity_downstream_start.py
@@ -2,19 +2,9 @@ import unittest
 from collections import OrderedDict
 from mock import patch
 from activity.activity_DownstreamStart import activity_DownstreamStart
-import tests.activity.settings_mock as settings_mock
 from tests.activity.classes_mock import FakeLogger, FakeSession
-
-
-def data():
-    data = OrderedDict()
-    data["run"] = "cf9c7e86-7355-4bb4-b48e-0bc284221251"
-    data["article_id"] = "30274"
-    data["version"] = "1"
-    data["workflow"] = "software_heritage"
-    data["recipient"] = "software_heritage"
-    data["input_file"] = "https://hub.stenci.la/api/projects/518/snapshots/15/archive"
-    return data
+import tests.activity.test_activity_data as testdata
+import tests.activity.settings_mock as settings_mock
 
 
 class TestDownstreamStart(unittest.TestCase):
@@ -28,7 +18,7 @@ class TestDownstreamStart(unittest.TestCase):
     def test_do_activity(self, fake_session):
         fake_session.return_value = FakeSession({})
 
-        result = self.activity.do_activity(data())
+        result = self.activity.do_activity(testdata.SoftwareHeritageDeposit_data_example)
         self.assertEqual(self.activity.ACTIVITY_SUCCESS, result)
 
         print(self.activity.logger.loginfo)
@@ -58,7 +48,7 @@ class TestDownstreamStart(unittest.TestCase):
     def test_do_activity_session_exception(self, fake_session):
         fake_session.side_effect = Exception("Exception in get_session")
 
-        result = self.activity.do_activity(data())
+        result = self.activity.do_activity(testdata.SoftwareHeritageDeposit_data_example)
         self.assertEqual(self.activity.ACTIVITY_PERMANENT_FAILURE, result)
         self.assertEqual(
             self.activity.logger.logexception,
