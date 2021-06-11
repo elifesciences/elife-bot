0ad641d3c0551f3883227be4b75351c2874a0700
Graham Nott <gnott@starglobal.ca>
2021-06-11 16:10:37 -0700

    Add activity to IngestAcceptedSubmission workflow.

--- workflow/workflow_IngestAcceptedSubmission.py
+++ workflow/workflow_IngestAcceptedSubmission.py
@@ -37,6 +37,7 @@ class workflow_IngestAcceptedSubmission(Workflow):
             "start": {"requirements": None},
             "steps": [
                 define_workflow_step("PingWorker", data),
+                define_workflow_step("ValidateAcceptedSubmission", data),
             ],
             "finish": {"requirements": None},
         }
