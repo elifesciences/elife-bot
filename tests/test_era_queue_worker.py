44063083e954bbafd743f70ca1bf9247339af4c7
Graham Nott <gnott@starglobal.ca>
2021-08-24 16:34:34 -0700

    Exception in era queue worker for bad JSON format.

--- tests/test_era_queue_worker.py
+++ tests/test_era_queue_worker.py
@@ -80,6 +80,46 @@ class TestEraQueueWorker(unittest.TestCase):
         out_queue_message = json.loads(directory.read("fake_sqs_body"))
         self.assertEqual(out_queue_message, outgoing_message_expected)
 
+    @patch("era_queue_worker.Message")
+    @patch("tests.activity.classes_mock.FakeSQSQueue.read")
+    @patch("boto.sqs.connect_to_region")
+    def test_work_bad_message_body(
+        self, fake_sqs_conn, fake_queue_read, fake_sqs_message
+    ):
+        "test if the incoming message body cannot be parsed into JSON"
+        message_body = '{"not": "good" "json": "this JSON has no commas"}'
+        # expected queue message will be similar to the incoming message,
+        # since it does not result in an outgoing message to the workflow starter queue
+        queue_message_expected = bytes(message_body, "utf8")
+
+        # mock things
+        directory = TempDirectory()
+        fake_sqs_conn.return_value = FakeSQSConn(directory)
+        fake_sqs_message.return_value = FakeSQSMessage(directory)
+
+        incoming_message = FakeSQSMessage(directory)
+        incoming_message.set_body(message_body)
+        fake_queue_read.return_value = incoming_message
+
+        # create a fake green flag
+        flag = FakeFlag()
+
+        # invoke queue worker to work
+        self.queue_worker.work(flag)
+
+        # assertions, there is no workflow starter message out_queue
+        out_queue_message = directory.read("fake_sqs_body")
+        self.assertEqual(out_queue_message, queue_message_expected)
+        # and exception is printed in the log
+        self.assertEqual(
+            self.logger.logexception,
+            (
+                "Exception loading message body as JSON: "
+                + str(queue_message_expected)
+                + ": Expecting ',' delimiter: line 1 column 16 (char 15)"
+            ),
+        )
+
     @patch("requests.head")
     def test_approve_workflow_start_200(self, mock_requests_head):
         mock_requests_head.return_value = FakeResponse(200)
