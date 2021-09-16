b2f8c26989600967f00fd5f0ae3bda2c6b4cf601
Graham Nott <gnott@starglobal.ca>
2021-09-16 09:51:21 -0700

    Refactor cron_NewS3POA.py

--- starter/cron_NewS3POA.py
+++ starter/cron_NewS3POA.py
@@ -1,8 +1,7 @@
 import boto.swf
-import log
-
 from provider import utils
 import starter.starter_helper as helper
+from starter.objects import Starter
 from S3utility.s3_notification_info import S3NotificationInfo
 from S3utility.s3_sqs_message import S3SQSMessage
 
@@ -14,23 +13,27 @@ Cron job to check for new article S3 POA and start workflows
 MAX_MESSAGE_COUNT = 10
 
 
-class cron_NewS3POA(object):
+class cron_NewS3POA(Starter):
+    def __init__(self, settings=None, logger=None):
+        super(cron_NewS3POA, self).__init__(settings, logger, "cron_NewS3POA")
 
     def start(self, settings):
+        """method for backwards compatibility"""
+        self.settings = settings
+        self.instantiate_logger()
+        self.start_workflow()
+
+    def start_workflow(self):
 
         ping_marker_id = "cron_NewS3POA"
 
-        # Log
-        logFile = "starter.log"
-        logger = log.logger(logFile, settings.setLevel, ping_marker_id)
-
         # Start a ping workflow as a marker
-        helper.start_ping_marker(ping_marker_id, settings, logger)
+        helper.start_ping_marker(ping_marker_id, self.settings, self.logger)
 
         # Get data from SQS queue
-        sqs_conn = sqs_connect(settings)
-        sqs_queue = get_sqs_queue(sqs_conn, settings)
-        process_queue(sqs_queue, settings, logger)
+        sqs_conn = sqs_connect(self.settings)
+        sqs_queue = get_sqs_queue(sqs_conn, self.settings)
+        process_queue(sqs_queue, self.settings, self.logger)
 
 
 def sqs_connect(settings):
@@ -38,7 +41,8 @@ def sqs_connect(settings):
     return boto.sqs.connect_to_region(
         settings.sqs_region,
         aws_access_key_id=settings.aws_access_key_id,
-        aws_secret_access_key=settings.aws_secret_access_key)
+        aws_secret_access_key=settings.aws_secret_access_key,
+    )
 
 
 def get_sqs_queue(sqs_conn, settings):
@@ -55,38 +59,42 @@ def process_queue(sqs_queue, settings, logger):
         try:
             messages = get_queue_messages(sqs_queue, MAX_MESSAGE_COUNT, logger)
         except:
-            logger.exception('Breaking process queue read loop, failed to get messages from queue')
+            logger.exception(
+                "Breaking process queue read loop, failed to get messages from queue"
+            )
             break
 
         # check if any messages to process
         if not messages:
-            logger.info('no messages available')
+            logger.info("no messages available")
             break
         else:
-            logger.info('Processing %s messages' % len(messages))
+            logger.info("Processing %s messages" % len(messages))
 
         # process each message, deleting each message when done
         for message in messages:
             # increment count
             message_count += 1
-            logger.info('Processing message number %s', message_count)
+            logger.info("Processing message number %s", message_count)
 
             # check message type
-            if message.notification_type != 'S3Event':
+            if message.notification_type != "S3Event":
                 logger.info(
-                    'Message not processed, deleting it from queue: %s' %
-                    message.get_body())
+                    "Message not processed, deleting it from queue: %s"
+                    % message.get_body()
+                )
                 sqs_queue.delete_message(message)
                 continue
 
             # start a workflow
             try:
-                logger.info('Starting workflow for message: %s' % message)
+                logger.info("Starting workflow for message: %s" % message)
                 start_package_poa_workflow(message, settings, logger)
             except:
                 logger.exception(
-                    'Exception processing message, deleting it from queue: %s' %
-                    message.get_body())
+                    "Exception processing message, deleting it from queue: %s"
+                    % message.get_body()
+                )
             finally:
                 sqs_queue.delete_message(message)
 
@@ -95,7 +103,7 @@ def get_queue_messages(sqs_queue, num_messages, logger):
     try:
         return sqs_queue.get_messages(num_messages)
     except:
-        logger.exception('Exception in getting messages from SQS queue')
+        logger.exception("Exception in getting messages from SQS queue")
         raise
 
 
@@ -111,9 +119,11 @@ def start_package_poa_workflow(sqs_message, settings, logger):
         helper.import_starter_module(starter_name, logger)
         starter_object = helper.get_starter_module(starter_name, logger)
         starter_object.start(settings=settings, document=document)
-        logger.info('Started %s workflow for document %s' % (starter_name, document))
+        logger.info("Started %s workflow for document %s" % (starter_name, document))
     except:
-        logger.exception('Error: starting %s for document %s' % (starter_name, document))
+        logger.exception(
+            "Error: starting %s for document %s" % (starter_name, document)
+        )
         raise
 
 
@@ -122,6 +132,6 @@ if __name__ == "__main__":
     ENV = utils.console_start_env()
     SETTINGS = utils.get_settings(ENV)
 
-    o = cron_NewS3POA()
+    STARTER = cron_NewS3POA()
 
-    o.start(settings=SETTINGS)
+    STARTER.start(settings=SETTINGS)
