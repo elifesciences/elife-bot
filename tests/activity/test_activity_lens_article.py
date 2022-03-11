1d5dbbf0ddb66a13d525bd5c1d83e1260d5f327c
Graham Nott <gnott@starglobal.ca>
2022-03-11 10:08:10 -0800

    Refactor LensArticle to use storage_provider.py

--- tests/activity/test_activity_lens_article.py
+++ tests/activity/test_activity_lens_article.py
@@ -2,13 +2,22 @@ import unittest
 import os
 import shutil
 from mock import patch
-import tests.activity.settings_mock as settings_mock
 from activity.activity_LensArticle import activity_LensArticle
 from provider.article import article
-from tests.activity.classes_mock import FakeLogger
-from tests.activity.classes_mock import FakeKey
-from tests.activity.classes_mock import FakeS3Connection
-from testfixtures import TempDirectory
+from tests.activity.classes_mock import FakeLogger, FakeStorageContext
+from tests.activity import helpers, settings_mock, test_activity_data
+
+
+def fake_download_xml(filename, to_dir):
+    source_doc = "tests/test_data/" + filename
+    dest_doc = os.path.join(to_dir, filename)
+    try:
+        shutil.copy(source_doc, dest_doc)
+        return filename
+    except IOError:
+        pass
+    # default return assume a failure
+    return False
 
 
 class TestLensArticle(unittest.TestCase):
@@ -19,37 +28,25 @@ class TestLensArticle(unittest.TestCase):
         )
 
     def tearDown(self):
-        TempDirectory.cleanup_all()
+        helpers.delete_files_in_folder(
+            test_activity_data.ExpandArticle_files_dest_folder, filter_out=[".gitkeep"]
+        )
         self.activity.clean_tmp_dir()
 
-    def fake_download_xml(self, filename, to_dir):
-        source_doc = "tests/test_data/" + filename
-        dest_doc = os.path.join(to_dir, filename)
-        try:
-            shutil.copy(source_doc, dest_doc)
-            return filename
-        except IOError:
-            pass
-        # default return assume a failure
-        return False
-
-    @patch("activity.activity_LensArticle.Key")
-    @patch("activity.activity_LensArticle.S3Connection")
+    @patch("activity.activity_LensArticle.storage_context")
     @patch.object(article, "download_article_xml_from_s3")
-    def test_do_activity(self, fake_download_article_xml, fake_s3_mock, fake_key_mock):
-        directory = TempDirectory()
+    def test_do_activity(self, fake_download_article_xml, fake_storage_context):
         input_data = {"article_id": "353"}
         article_xml_file = "elife-00353-v1.xml"
-        article_s3key = "/00353/index.html"
+        article_s3key = "00353/index.html"
         expected_html_contains = (
             "https://cdn.elifesciences.org/articles/00353/elife-00353-v1.xml"
         )
 
-        fake_download_article_xml.return_value = self.fake_download_xml(
+        fake_download_article_xml.return_value = fake_download_xml(
             article_xml_file, self.activity.get_tmp_dir()
         )
-        fake_s3_mock.return_value = FakeS3Connection()
-        fake_key_mock.return_value = FakeKey(directory, "index.html")
+        fake_storage_context.return_value = FakeStorageContext()
 
         success = self.activity.do_activity(input_data)
         self.assertEqual(success, True)
@@ -66,22 +63,18 @@ class TestLensArticle(unittest.TestCase):
         success = self.activity.do_activity(input_data)
         self.assertEqual(success, True)
 
-    @patch("activity.activity_LensArticle.Key")
-    @patch("activity.activity_LensArticle.S3Connection")
+    @patch("activity.activity_LensArticle.storage_context")
     @patch.object(article, "download_article_xml_from_s3")
     def test_do_activity_poa_article(
-        self, fake_download_article_xml, fake_s3_mock, fake_key_mock
+        self, fake_download_article_xml, fake_storage_context
     ):
         "for test coverage try a POA article"
-        directory = TempDirectory()
         input_data = {"article_id": "3977"}
         article_xml_file = "elife_poa_e03977.xml"
-        article_s3key = ""
-        fake_download_article_xml.return_value = self.fake_download_xml(
+        fake_download_article_xml.return_value = fake_download_xml(
             article_xml_file, self.activity.get_tmp_dir()
         )
-        fake_s3_mock.return_value = FakeS3Connection()
-        fake_key_mock.return_value = FakeKey(directory, article_s3key)
+        fake_storage_context.return_value = FakeStorageContext()
 
         success = self.activity.do_activity(input_data)
         self.assertEqual(success, True)
@@ -90,7 +83,3 @@ class TestLensArticle(unittest.TestCase):
         "test coverage of passing None"
         article_html = self.activity.get_article_html(None, None, None, None)
         self.assertIsNotNone(article_html)
-
-
-if __name__ == "__main__":
-    unittest.main()
