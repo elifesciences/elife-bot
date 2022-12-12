572121f53c3bfea0db63f70611518dc9f5a377d7
Luke <lsh-0@users.noreply.github.com>
2022-12-12 14:43:51 +1030

    install.sh, cleans any psutil .so files

--- install.sh
+++ install.sh
@@ -17,6 +17,10 @@ find workflow/ -maxdepth 1 -name '*.pyc' -delete
 find starter/ -maxdepth 1 -name '*.pyc' -delete
 find S3utility/ -maxdepth 1 -name '*.pyc' -delete
 
+# lsh@2022-12-12: upgrading from psutil 5.9.3 to 5.9.4 did not remove .so files compiled for 5.9.3
+# switch_revision_update_instance later dies with and "ImportError: version conflict" when calling register.py
+rm -f venv/lib/python*/site-packages/psutil/*.so
+
 source venv/bin/activate
 
 # 'pip' fixes issues installing wheel packages, hides deprecation warnings
