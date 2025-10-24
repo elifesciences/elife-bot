51d6067a37016fc3f2564cd46afa0332b03af9b0
Graham Nott <gnott@starglobal.ca>
2025-10-24 14:28:47 -0700

    Add endpoint retry logic to ValidatePreprintSchematron

--- activity/activity_ValidatePreprintSchematron.py
+++ activity/activity_ValidatePreprintSchematron.py
@@ -1,6 +1,7 @@
 import os
 import json
 import re
+import time
 from provider import github_provider, meca
 from provider.execution_context import get_session
 from provider.storage_provider import storage_context
@@ -8,6 +9,15 @@ from activity.objects import Activity
 
 
 ENHANCE_MESSAGE = True
+
+# session variable name to store the number of attempts
+SESSION_ATTEMPT_COUNTER_NAME = "validate_preprint_schematron_attempt_count"
+
+# maximum endpoint request attempts
+MAX_ATTEMPTS = 4
+
+# time in seconds to sleep between endpoint request attempts
+SLEEP_SECONDS = 10
 
 
 class activity_ValidatePreprintSchematron(Activity):
@@ -126,10 +136,45 @@ class activity_ValidatePreprintSchematron(Activity):
                 % (self.name, endpoint_url, xml_file_path)
             )
             self.logger.exception(log_message)
-            # add github issue comment
-            github_provider.add_github_issue_comment(
-                self.settings, self.logger, self.name, version_doi, log_message
-            )
+
+            # count the number of attempts
+            if not session.get_value(SESSION_ATTEMPT_COUNTER_NAME):
+                session.store_value(SESSION_ATTEMPT_COUNTER_NAME, 1)
+            else:
+                # increment
+                session.store_value(
+                    SESSION_ATTEMPT_COUNTER_NAME,
+                    int(session.get_value(SESSION_ATTEMPT_COUNTER_NAME)) + 1,
+                )
+                self.logger.info(
+                    "%s, POST to endpoint_url attempts for file %s: %s"
+                    % (
+                        self.name,
+                        xml_file_path,
+                        session.get_value(SESSION_ATTEMPT_COUNTER_NAME),
+                    )
+                )
+
+            if int(session.get_value(SESSION_ATTEMPT_COUNTER_NAME)) < MAX_ATTEMPTS:
+                # Clean up disk
+                self.clean_tmp_dir()
+                # sleep a short time
+                time.sleep(SLEEP_SECONDS)
+                # return a temporary failure
+                return self.ACTIVITY_TEMPORARY_FAILURE
+            if int(session.get_value(SESSION_ATTEMPT_COUNTER_NAME)) >= MAX_ATTEMPTS:
+                # maximum number of attempts are completed
+
+                # add github issue comment
+                github_provider.add_github_issue_comment(
+                    self.settings, self.logger, self.name, version_doi, log_message
+                )
+
+                # add log message about exceeding retries
+                self.logger.exception(
+                    "%s, POST to endpoint_url %s attempts reached MAX_ATTEMPTS of %s for file %s"
+                    % (self.name, endpoint_url, MAX_ATTEMPTS, xml_file_path)
+                )
 
         self.logger.info(
             "%s, statuses for version DOI %s: %s"
